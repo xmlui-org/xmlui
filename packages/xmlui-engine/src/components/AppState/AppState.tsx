53b113b6c292b7c24da5c949a2099f68f69a8ed0
Istvan Novak <dotneteer@hotmail.com>
2024-08-08 16:28:32 +0200

    docs: add AppState documentation

--- packages/xmlui-engine/src/components/AppState/AppState.tsx
+++ packages/xmlui-engine/src/components/AppState/AppState.tsx
@@ -37,11 +37,33 @@ function AppState({
   return null;
 }
 
-interface AppStateComponentDef extends ComponentDef<"AppState"> {
+/**
+ * AppState is a functional component (without a visible user interface) that helps store and 
+ * manage the app's state.
+ */
+export interface AppStateComponentDef extends ComponentDef<"AppState"> {
   props: {
+    /**
+     * This property is the identifier of the bucket to which the \`AppState\` instance is bound. 
+     * Multiple \`AppState\` instances with the same bucket will share the same state object: any of 
+     * them updating the state will cause the other instances to view the new, updated state.
+     */
     bucket?: string;
+    /**
+     * This property contains the initial state value. Though you can use multiple \`AppState\` 
+     * component instances for the same bucket with their \`initialValue\` set, it may result in faulty 
+     * app logic. When xmlui instantiates an \`AppInstance\` with an explicit initial value, that value 
+     * is immediately set. Multiple initial values may result in undesired initialization.
+     */
     initialValue?: string;
   };
+  api: {
+    /**
+     * You can access the state object through this property.
+     */
+    value: Record<string, any>;
+    update: (patch: Record<string, any>) => void;
+  }
 }
 
 export const appStateComponentRenderer = createComponentRenderer<AppStateComponentDef>(
