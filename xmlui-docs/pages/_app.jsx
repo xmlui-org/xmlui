d3f595b2f3a9b2bdc8306fadd2ebd4479c467daf
Marcell Kiss <52428841+marcello-tk@users.noreply.github.com>
2024-08-15 17:16:08 +0200

    prot: update docs site styling to be more in line with tabler (#42)
    Note: work in progress, textMate tokens need to be recolored, some paddings could be tweaked

---
+++ xmlui-docs/pages/_app.jsx
@@ -0,0 +1,13 @@
+import "../styles/global.scss";
+import { Inter } from 'next/font/google';
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
+
+export default function App({Component, pageProps}) {
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    );
+}
