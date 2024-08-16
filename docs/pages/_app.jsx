c636e632e1d4be965ebc12f1b245752c44b0e5ae
Istvan Novak <dotneteer@hotmail.com>
2024-08-16 13:49:26 +0200

    docs: experimenting with the Inter font

--- docs/pages/_app.jsx
+++ docs/pages/_app.jsx
@@ -1,13 +1,18 @@
 import "../styles/global.scss";
-import { Inter } from 'next/font/google';
- 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
 
-export default function App({Component, pageProps}) {
-    return (
-        <main className={inter.className}>
-            <Component {...pageProps} />
-        </main>
-    );
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+  style: ["normal"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main style={{fontFamily: inter.style.fontFamily}}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
