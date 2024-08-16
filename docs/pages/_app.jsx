8aeac587942a2e0dfe652c3d03315318d9eec2dc
Istvan Novak <dotneteer@hotmail.com>
2024-08-16 11:45:58 +0200

    docs: update font feature settings (2)

--- docs/pages/_app.jsx
+++ docs/pages/_app.jsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function App({Component, pageProps}) {
     return (
-        <main className={inter.className} style={{fontFeatureSettings: "'cv03', 'cv04', 'cv11'"}}>
+        <main className={inter.className}>
             <Component {...pageProps} />
         </main>
     );
