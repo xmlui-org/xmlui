b2445bf34a1514a865f391bf41333fb40e3dbe34
gergelyilles <illes.gergo@gmail.com>
2024-08-16 09:53:38 +0200

    fix build, tests

--- xmlui-engine/vitest.config.ts
+++ xmlui-engine/vitest.config.ts
@@ -1,8 +1,11 @@
 import viteConfig from "./vite.config";
 import { defineConfig, mergeConfig } from "vitest/config";
 
-export default mergeConfig(viteConfig, defineConfig({
-  test: {
-    include: ["**/tests/**/*.test.ts"],
-  },
-}));
+export default defineConfig(configEnv => mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        include: ["**/tests/**/*.test.ts"],
+      },
+    })
+));
