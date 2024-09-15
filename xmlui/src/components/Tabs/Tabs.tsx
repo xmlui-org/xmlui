f2133791cb2bf45f82c9bdff984cf1fe8d25a541
captaaint <56363291+captaaint@users.noreply.github.com>
2024-09-15 18:16:44 +0200

    wip

--- xmlui/src/components/Tabs/Tabs.tsx
+++ xmlui/src/components/Tabs/Tabs.tsx
@@ -7,51 +7,59 @@ import { parseScssVar } from "@components-core/theming/themeVars";
 import styles from "./Tabs.module.scss";
 import { createComponentRenderer } from "@components-core/renderers";
 import { MemoizedItem } from "@components/container-helpers";
-
-type TabItem = {
-  label: string;
-  content: ReactNode;
-};
+import { TabContext, useTabContextValue } from "@components/Tabs/TabContext";
 
 type TabsProps = {
-  tabs: TabItem[];
   activeTab?: number;
   orientation?: "horizontal" | "vertical";
   tabRenderer?: (item: { label: string; isActive: boolean }) => ReactNode;
   style?: CSSProperties;
+  children?: ReactNode;
 };
 
-const Tabs = ({ tabs = [], activeTab = 0, orientation = "vertical", tabRenderer, style }: TabsProps) => {
-  const _activeTab = activeTab < 0 ? 0 : activeTab > tabs.length - 1 ? tabs.length - 1 : activeTab;
+const Tabs = ({
+  activeTab = 0,
+  orientation = "vertical",
+  tabRenderer,
+  style,
+  children,
+}: TabsProps) => {
+  const { tabItems, tabContextValue } = useTabContextValue();
+  const _activeTab =
+    activeTab <= 0 ? 0 : activeTab > tabItems.length - 1 ? tabItems.length - 1 : activeTab;
   const [currentTab, setCurrentTab] = useState(`${_activeTab}`);
+
   return (
-    <RTabs.Root
-      className={styles.tabs}
-      value={`${currentTab}`}
-      onValueChange={setCurrentTab}
-      orientation={orientation}
-      style={style}
-    >
-      <RTabs.List className={styles.tabsList}>
-        {tabs.map((tab, index) =>
-          tabRenderer ? (
-            <RTabs.Trigger key={index} value={`${index}`}>
-              {tabRenderer({ label: tab.label, isActive: `${index}` === currentTab })}
-            </RTabs.Trigger>
-          ) : (
-            <RTabs.Trigger className={styles.tabTrigger} key={index} value={`${index}`} >
-              {tab.label}
-            </RTabs.Trigger>
-          )
-        )}
-        <div className={styles.filler} data-orientation={orientation}/>
-      </RTabs.List>
-      {tabs.map((tab, index) => (
-        <RTabs.Content key={index} value={`${index}`} className={styles.tabsContent}>
-          {tab.content}
-        </RTabs.Content>
-      ))}
-    </RTabs.Root>
+    <TabContext.Provider value={tabContextValue}>
+      {children}
+      <RTabs.Root
+        className={styles.tabs}
+        value={`${currentTab}`}
+        onValueChange={setCurrentTab}
+        orientation={orientation}
+        style={style}
+      >
+        <RTabs.List className={styles.tabsList}>
+          {tabItems.map((tab, index) =>
+            tabRenderer ? (
+              <RTabs.Trigger key={index} value={`${index}`}>
+                {tabRenderer({ label: tab.label, isActive: `${index}` === currentTab })}
+              </RTabs.Trigger>
+            ) : (
+              <RTabs.Trigger className={styles.tabTrigger} key={index} value={`${index}`}>
+                {tab.label}
+              </RTabs.Trigger>
+            ),
+          )}
+          <div className={styles.filler} data-orientation={orientation} />
+        </RTabs.List>
+        {tabItems.map((tab, index) => (
+          <RTabs.Content key={index} value={`${index}`} className={styles.tabsContent}>
+            {tab.content}
+          </RTabs.Content>
+        ))}
+      </RTabs.Root>
+    </TabContext.Provider>
   );
 };
 
@@ -62,8 +70,6 @@ const Tabs = ({ tabs = [], activeTab = 0, orientation = "vertical", tabRenderer,
  */
 export interface TabsComponentDef extends ComponentDef<"Tabs"> {
   props: {
-    /** @internal */
-    tabs: TabItem[];
     /**
      * Indicates which tab index is active. The indexing starts from 0. The default value is 0 (aka the first tab from left to right).
      * @descriptionRef
@@ -82,6 +88,8 @@ export interface TabsComponentDef extends ComponentDef<"Tabs"> {
      * @descriptionRef
      */
     tabTemplate?: ComponentDef;
+
+    children?: ReactNode;
   };
 }
 
@@ -89,7 +97,6 @@ export const TabsMd: ComponentDescriptor<TabsComponentDef> = {
   displayName: "Tabs",
   description: "Tabs component",
   props: {
-    tabs: desc("The tabs to be displayed in the tab component"),
     activeTab: desc("The active tab index"),
     orientation: desc("The orientation of the tabs"),
     tabTemplate: desc("The template for the tab"),
@@ -114,31 +121,26 @@ export const TabsMd: ComponentDescriptor<TabsComponentDef> = {
 export const tabsComponentRenderer = createComponentRenderer<TabsComponentDef>(
   "Tabs",
   ({ extractValue, node, renderChild, layoutCss }) => {
-    const tabs: TabItem[] = [];
-    if (Array.isArray(node.children)) {
-      node.children.forEach((child) => {
-        if (child.type === "TabItem") {
-          tabs.push({
-            label: child.props?.label,
-            content: renderChild(child.children),
-          });
-        }
-      });
-    }
-
     return (
       <Tabs
         style={layoutCss}
         tabRenderer={
           !!node?.props?.tabTemplate
-            ? (item) => <MemoizedItem node={node.props.tabTemplate!} item={item} renderChild={renderChild} />
+            ? (item) => (
+                <MemoizedItem
+                  node={node.props.tabTemplate!}
+                  item={item}
+                  renderChild={renderChild}
+                />
+              )
             : undefined
         }
-        tabs={tabs}
         activeTab={extractValue(node.props?.activeTab)}
         orientation={extractValue(node.props?.orientation)}
-      />
+      >
+        {renderChild(node.children)}
+      </Tabs>
     );
   },
-  TabsMd
+  TabsMd,
 );
