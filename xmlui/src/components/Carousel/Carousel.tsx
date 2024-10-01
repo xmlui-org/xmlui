34bf33e84a6e883e0b11b505a7c097f3b252d24d
captaaint <56363291+captaaint@users.noreply.github.com>
2024-10-01 22:23:24 +0200

    wip: Carousel component - apis, events (#219)

--- xmlui/src/components/Carousel/Carousel.tsx
+++ xmlui/src/components/Carousel/Carousel.tsx
@@ -2,17 +2,77 @@ import { createMetadata, d } from "@abstractions/ComponentDefs";
 
 import { createComponentRenderer } from "@components-core/renderers";
 import { CarouselComponent } from "./CarouselNative";
+import { parseScssVar } from "@components-core/theming/themeVars";
+import styles from "@components/Carousel/Carousel.module.scss";
+import { dDidChange } from "@components/metadata-helpers";
 
 const COMP = "Carousel";
 
 export const CarouselMd = createMetadata({
   status: "in progress",
+  props: {
+    orientation: d(
+      "This property indicates the orientation of the carousel. The `horizontal` value indicates that the carousel moves horizontally, and the `vertical` value indicates that the carousel moves vertically.",
+      ["horizontal", "vertical"],
+      null,
+      "horizontal",
+    ),
+    indicators: d(
+      "This property indicates whether the carousel displays the indicators.",
+      null,
+      null,
+      "true",
+    ),
+  },
+  events: {
+    displayDidChange: dDidChange(COMP),
+  },
+  apis: {
+    canScrollPrev: d("This method returns `true` if the carousel can scroll to the previous slide."),
+    canScrollNext: d("This method returns `true` if the carousel can scroll to the next slide."),
+    scrollTo: d("This method scrolls the carousel to the specified slide index."),
+    scrollPrev: d("This method scrolls the carousel to the previous slide."),
+    scrollNext: d("This method scrolls the carousel to the next slide."),
+  },
+  themeVars: parseScssVar(styles.themeVars),
+  defaultThemeVars: {
+    [`color-bg-control-${COMP}`]: "$color-bg-primary",
+    [`color-text-control-${COMP}`]: "$color-text-primary",
+    [`color-bg-control-hover-${COMP}`]: "$color-bg-primary",
+    [`color-text-control-hover-${COMP}`]: "$color-text-primary",
+    [`color-bg-control-active-${COMP}`]: "$color-bg-primary",
+    [`color-bg-control-disabled-${COMP}`]: "$color-surface-200",
+    [`color-text-control-disabled-${COMP}`]: "$color-text-disabled",
+    [`color-text-control-active-${COMP}`]: "$color-text-primary",
+    [`color-bg-indicator-${COMP}`]: "$color-surface-200",
+    [`color-bg-indicator-active-${COMP}`]: "$color-bg-primary",
+    [`color-text-indicator-${COMP}`]: "$color-text-primary",
+    [`color-text-indicator-active-${COMP}`]: "$color-text-primary",
+    [`color-bg-indicator-hover-${COMP}`]: "$color-surface-200",
+    [`color-text-indicator-hover-${COMP}`]: "$color-text-primary",
+    [`width-indicator-${COMP}`]: "25px",
+    [`height-indicator-${COMP}`]: "6px",
+    [`height-control-${COMP}`]: "36px",
+    [`width-control-${COMP}`]: "36px",
+    [`radius-control-${COMP}`]: "50%",
+    [`height-${COMP}`]: "100%",
+    [`width-${COMP}`]: "100%",
+  },
 });
 
 export const carouselComponentRenderer = createComponentRenderer(
   COMP,
   CarouselMd,
-  ({ node, renderChild, layoutCss }) => {
-    return <CarouselComponent style={layoutCss}>{renderChild(node.children)}</CarouselComponent>;
+  ({ node, renderChild, layoutCss, extractValue, lookupEventHandler, registerComponentApi }) => {
+    return (
+      <CarouselComponent
+        style={layoutCss}
+        orientation={extractValue(node.props?.orientation)}
+        onDisplayDidChange={lookupEventHandler("displayDidChange")}
+        registerComponentApi={registerComponentApi}
+      >
+        {renderChild(node.children)}
+      </CarouselComponent>
+    );
   },
 );
--- xmlui/src/components/Carousel/CarouselNative.tsx
+++ xmlui/src/components/Carousel/CarouselNative.tsx
@@ -3,48 +3,55 @@ import classnames from "@components-core/utils/classnames";
 import useEmblaCarousel, { type UseEmblaCarouselType } from "embla-carousel-react";
 import { CarouselContext, useCarouselContextValue } from "@components/Carousel/CarouselContext";
 import styles from "./Carousel.module.scss";
-import { CSSProperties, useRef } from "react";
+import { CSSProperties, useCallback, useEffect, useRef } from "react";
 import Icon from "@components/Icon/IconNative";
+import { noop } from "@components-core/constants";
+import type { RegisterComponentApiFn } from "@abstractions/RendererDefs";
 
 type CarouselApi = UseEmblaCarouselType[1];
-type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
-type CarouselOptions = UseCarouselParameters[0];
-type CarouselPlugin = UseCarouselParameters[1];
 
 export type CarouselProps = {
   style?: CSSProperties;
-  opts?: CarouselOptions;
-  plugins?: CarouselPlugin;
   orientation?: "horizontal" | "vertical";
-  setApi?: (api: CarouselApi) => void;
+  indicators?: boolean;
+  children: React.ReactNode;
+  onDisplayDidChange?: (activeSlide: number) => void;
+  registerComponentApi?: RegisterComponentApiFn;
 };
 
 const CarouselComponent = ({
   orientation = "horizontal",
-  opts,
-  setApi,
-  plugins,
-  className,
   children,
   style,
-}: any) => {
+  indicators = true,
+  onDisplayDidChange = noop,
+  registerComponentApi,
+}: CarouselProps) => {
   const ref = useRef(null);
   const { carouselItems, carouselContextValue } = useCarouselContextValue();
-  const [carouselRef, api] = useEmblaCarousel(
-    {
-      ...opts,
-      axis: orientation === "horizontal" ? "x" : "y",
+  const [activeSlide, setActiveSlide] = React.useState(0);
+  const [carouselRef, api] = useEmblaCarousel({
+    axis: orientation === "horizontal" ? "x" : "y",
+  });
+
+  const scrollTo = useCallback(
+    (index: number) => {
+      api?.scrollTo(index);
     },
-    plugins,
+    [api],
   );
+
   const [canScrollPrev, setCanScrollPrev] = React.useState(false);
   const [canScrollNext, setCanScrollNext] = React.useState(false);
-  const carouselOrientation = orientation || (opts?.axis === "y" ? "vertical" : "horizontal");
 
   const onSelect = React.useCallback((api: CarouselApi) => {
     if (!api) {
       return;
     }
+
+    const activeIndex = api.selectedScrollSnap();
+    onDisplayDidChange(activeIndex);
+    setActiveSlide(activeIndex);
 
     setCanScrollPrev(api.canScrollPrev());
     setCanScrollNext(api.canScrollNext());
@@ -71,13 +78,15 @@ const CarouselComponent = ({
     [scrollPrev, scrollNext],
   );
 
-  React.useEffect(() => {
-    if (!api || !setApi) {
-      return;
-    }
-
-    setApi(api);
-  }, [api, setApi]);
+  useEffect(() => {
+    registerComponentApi?.({
+      scrollTo,
+      scrollPrev,
+      scrollNext,
+      canScrollPrev: () => canScrollPrev,
+      canScrollNext: () => canScrollNext,
+    });
+  }, [registerComponentApi, scrollTo, scrollPrev, scrollNext, canScrollPrev, canScrollNext]);
 
   React.useEffect(() => {
     if (!api) {
@@ -100,68 +109,69 @@ const CarouselComponent = ({
         style={style}
         ref={ref}
         onKeyDownCapture={handleKeyDown}
-        className={classnames(styles.carousel, className)}
+        className={classnames(styles.carousel)}
         role="region"
         aria-roledescription="carousel"
       >
-        <button
-          className={classnames(
-            styles.controlButton,
-            {
-              [styles.controlPrevHorizontal]: carouselOrientation === "horizontal",
-              [styles.controlPrevVertical]: carouselOrientation === "vertical",
-            },
-            className,
-          )}
-          disabled={!canScrollPrev}
-          onClick={scrollPrev}
-        >
-          <Icon name={"arrowleft"} />
-        </button>
         <div ref={carouselRef} className={styles.carouselContentWrapper}>
           <div
-            className={classnames(
-              styles.carouselContent,
-              {
-                [styles.horizontal]: carouselOrientation === "horizontal",
-                [styles.vertical]: carouselOrientation === "vertical",
-              },
-              className,
-            )}
+            className={classnames(styles.carouselContent, {
+              [styles.horizontal]: orientation === "horizontal",
+              [styles.vertical]: orientation === "vertical",
+            })}
           >
             {carouselItems.map((item, _index) => (
               <div
                 key={_index}
                 role="group"
                 aria-roledescription="slide"
-                className={classnames(
-                  styles.carouselItem,
-                  {
-                    [styles.itemHorizontal]: carouselOrientation === "horizontal",
-                    [styles.itemVertical]: carouselOrientation === "vertical",
-                  },
-                  className,
-                )}
+                className={classnames(styles.carouselItem, {
+                  [styles.itemHorizontal]: orientation === "horizontal",
+                  [styles.itemVertical]: orientation === "vertical",
+                })}
               >
                 {item.content}
               </div>
             ))}
           </div>
         </div>
-        <button
-          className={classnames(
-            styles.controlButton,
-            {
-              [styles.controlNextHorizontal]: carouselOrientation === "horizontal",
-              [styles.controlNextVertical]: carouselOrientation === "vertical",
-            },
-            className,
-          )}
-          onClick={scrollNext}
-          disabled={!canScrollNext}
-        >
-          <Icon name={"arrowright"} />
-        </button>
+        <div className={styles.controls}>
+          <button
+            className={classnames(styles.controlButton, {
+              [styles.controlPrevHorizontal]: orientation === "horizontal",
+              [styles.controlPrevVertical]: orientation === "vertical",
+            })}
+            disabled={!canScrollPrev}
+            onClick={scrollPrev}
+          >
+            <Icon name={"arrowleft"} />
+          </button>
+          <button
+            className={classnames(styles.controlButton, {
+              [styles.controlNextHorizontal]: orientation === "horizontal",
+              [styles.controlNextVertical]: orientation === "vertical",
+            })}
+            onClick={scrollNext}
+            disabled={!canScrollNext}
+          >
+            <Icon name={"arrowright"} />
+          </button>
+        </div>
+        {indicators && (
+          <div className={styles.indicators}>
+            {carouselItems.map((_, index) => (
+              <button
+                key={index}
+                type="button"
+                onClick={() => scrollTo(index)}
+                className={classnames(styles.indicator, {
+                  [styles.active]: index === activeSlide,
+                })}
+                aria-current={index === activeSlide}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </CarouselContext.Provider>
   );
