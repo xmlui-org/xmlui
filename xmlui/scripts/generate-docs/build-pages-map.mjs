c15a1b904260c18ffbe38df986348c902bf5a762
marcello-tk <awesomedevguynull@gmail.com>
2024-10-07 17:53:49 +0100

    feat: add subheadings as constants

--- xmlui/scripts/generate-docs/build-pages-map.mjs
+++ xmlui/scripts/generate-docs/build-pages-map.mjs
@@ -27,9 +27,9 @@ export function buildPagesMap(pagesFolder, outFilePathAndName) {
           acceptedExtensions.includes(extname(item.name))) &&
         !rejectedFileNames.includes(item.name)
       ) {
-        const articleHeading = getArticleIds(item);
-        if (articleHeading) {
-          pages.push(articleHeading);
+        const articleHeadings = getArticleIds(item);
+        if (articleHeadings) {
+          pages.push(...articleHeadings);
         }
       }
     }
@@ -82,7 +82,15 @@ function getArticleIds(article) {
 
   const titleId = getTitleId(lines);
   if (!titleId) return null;
-  return { id: titleId, path: relativeArticlePath };
+
+  const subHeadingIds = getSubHeadingIds(lines);
+  return [
+    { id: toHeadingId(titleId), path: relativeArticlePath },
+    ...subHeadingIds.map((id) => ({
+      id: `${toHeadingId(titleId)}_${toHeadingId(id)}`,
+      path: `${relativeArticlePath}#${toHeadingPath(id)}`,
+    })),
+  ];
 
   // ---
 
@@ -92,36 +100,43 @@ function getArticleIds(article) {
       if (!match) continue;
       if (match[1]) {
         // Has ID, extract it and use that
-        return normalizeToHeadingId(match[1].replace(/\[#(.*?)\]/, (_, p1) => p1));
+        return match[1].replace(/\[#(.*?)\]/, (_, p1) => p1);
       } else {
         // Generate new ID from the heading title
-        return normalizeToHeadingId(match[0].slice(1));
+        return match[0].slice(1);
       }
     }
   }
 
   function getSubHeadingIds(lines) {
+    const headings = [];
     for (const line of lines) {
       // We only gather headings which have an explicit ID defined and they are not leveled as h1
       const match = line.match(/^##+\s+.+?\s*(\[#[\w-]+\])$/);
       if (!match) continue;
-      if (match[1]) {
-        // Has ID, extract it and use that
-        return normalizeToHeadingId(match[1].replace(/\[#(.*?)\]/, (_, p1) => p1));
-      } else {
-        // Generate new ID from the heading title
-        return normalizeToHeadingId(match[0].slice(1));
-      }
+      if (!match[1]) continue;
+      // Has ID, extract it and use that
+      headings.push(match[1].replace(/\[#(.*?)\]/, (_, p1) => p1));
     }
+    return headings;
   }
 }
 
-function normalizeToHeadingId(rawStr) {
+function toHeadingId(rawStr) {
   return rawStr
     .trim()
     .toLocaleUpperCase()
     .replaceAll(/[^A-Za-z0-9_]/g, "_")
     .replaceAll(/__+/g, "_"); // <- remove duplicate underscores
+}
+
+function toHeadingPath(rawStr) {
+  return rawStr
+    .trim()
+    .toLocaleLowerCase()
+    .replaceAll(/[^A-Za-z0-9-]/g, "-")
+    .replaceAll(/--+/g, "-")
+    .replace(/^-|-$/, "");
 }
 
 function indicateAndRemoveDuplicateIds(pagesData) {
