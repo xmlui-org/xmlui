a070486fd45db2720cc254c7a2291be55c1a41cd
marcello-tk <awesomedevguynull@gmail.com>
2024-10-18 13:09:38 +0100

    docs: add basic component batch #2, fix comp metadata
    - components: TextBox, TextArea, Select, MultiSelect, NumberBox
    - rework other sections, add how-to's

--- docs/meta/downloads.js
+++ docs/meta/downloads.js
@@ -1,2 +1,2 @@
-export const GETTING_STARTED = "/getting-started/getting-started.zip";
 export const CL_TUTORIAL = "/getting-started/cl-tutorial.zip";
+export const GETTING_STARTED = "/getting-started/getting-started.zip";
