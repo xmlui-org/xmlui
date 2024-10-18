0dbcafc894e37834ea895b74c9bc65b9877c30c4
Istvan Novak <dotneteer@hotmail.com>
2024-10-18 07:42:25 +0200

    wip: update Getting Started

--- xmlui/src/components/List/List.tsx
+++ xmlui/src/components/List/List.tsx
@@ -100,6 +100,7 @@ export const dynamicHeightListComponentRenderer = createComponentRenderer(
     registerComponentApi
   }) => {
       const itemTemplate = node.props.itemTemplate || node.children;
+      console.log("itemTemplate", itemTemplate);
       return (
       <DynamicHeightList
         registerComponentApi={registerComponentApi}
