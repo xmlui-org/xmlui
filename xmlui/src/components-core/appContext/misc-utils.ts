e1edc6b9b318aa770cae149257ae3344ddc9ebf8
Istvan Novak <dotneteer@hotmail.com>
2024-10-21 07:13:08 +0200

    docs: update Contact List according to Nathan's review

--- xmlui/src/components-core/appContext/misc-utils.ts
+++ xmlui/src/components-core/appContext/misc-utils.ts
@@ -1,11 +1,17 @@
 import { capitalize, defaultTo } from "lodash-es";
 
-import { formatFileSizeInBytes, getFileExtension, pluralize } from "@components-core/utils/misc";
+import {
+  formatFileSizeInBytes,
+  getFileExtension,
+  pluralize,
+  toHashObject,
+} from "@components-core/utils/misc";
 
 export const miscellaneousUtils = {
   formatFileSizeInBytes,
   getFileExtension,
   capitalize,
   pluralize,
-  defaultTo
+  defaultTo,
+  toHashObject,
 };
