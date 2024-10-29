8d5df355fff4cd1b925311d64c043a6bfe4c0d8a
Marcell Kiss <52428841+marcello-tk@users.noreply.github.com>
2024-10-29 16:05:42 +0000

    docs: add Working with Resources article (#328)
    omit the automatic resource detection section for now -> need to make it work first

--- docs/meta/downloads.js
+++ docs/meta/downloads.js
@@ -1,8 +1,8 @@
-export const CL_TUTORIAL = "/getting-started/cl-tutorial.zip";
 export const CL_TUTORIAL_2 = "/getting-started/cl-tutorial-2.zip";
 export const CL_TUTORIAL_3 = "/getting-started/cl-tutorial-3.zip";
 export const CL_TUTORIAL_4 = "/getting-started/cl-tutorial-4.zip";
 export const CL_TUTORIAL_5 = "/getting-started/cl-tutorial-5.zip";
 export const CL_TUTORIAL_6 = "/getting-started/cl-tutorial-6.zip";
 export const CL_TUTORIAL_FINAL = "/getting-started/cl-tutorial-final.zip";
+export const CL_TUTORIAL = "/getting-started/cl-tutorial.zip";
 export const GETTING_STARTED = "/getting-started/getting-started.zip";
