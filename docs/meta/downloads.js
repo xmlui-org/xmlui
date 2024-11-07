20de7eeac7b3fab40262d18f0e8161bfa4ff8108
Marcell Kiss <52428841+marcello-tk@users.noreply.github.com>
2024-11-07 08:18:47 +0000

    docs test: remove DataSource article #2 (#371)

--- docs/meta/downloads.js
+++ docs/meta/downloads.js
@@ -1,8 +1,8 @@
-export const CL_TUTORIAL_2 = "/getting-started/cl-tutorial2.zip";
-export const CL_TUTORIAL_3 = "/getting-started/cl-tutorial3.zip";
-export const CL_TUTORIAL_4 = "/getting-started/cl-tutorial4.zip";
-export const CL_TUTORIAL_5 = "/getting-started/cl-tutorial5.zip";
-export const CL_TUTORIAL_6 = "/getting-started/cl-tutorial6.zip";
 export const CL_TUTORIAL_FINAL = "/getting-started/cl-tutorial-final.zip";
 export const CL_TUTORIAL = "/getting-started/cl-tutorial.zip";
+export const CL_TUTORIAL2 = "/getting-started/cl-tutorial2.zip";
+export const CL_TUTORIAL3 = "/getting-started/cl-tutorial3.zip";
+export const CL_TUTORIAL4 = "/getting-started/cl-tutorial4.zip";
+export const CL_TUTORIAL5 = "/getting-started/cl-tutorial5.zip";
+export const CL_TUTORIAL6 = "/getting-started/cl-tutorial6.zip";
 export const GETTING_STARTED = "/getting-started/getting-started.zip";
