4abe615c8edaff4d36be6c6fd0ec77063a04aa9b
marcello-tk <awesomedevguynull@gmail.com>
2024-11-11 12:20:14 +0000

    docs: review component examples #2

--- xmlui/src/components/Text/TextNative.tsx
+++ xmlui/src/components/Text/TextNative.tsx
@@ -6,6 +6,7 @@ import styles from "./Text.module.scss";
 import classnames from "@components-core/utils/classnames";
 import { getMaxLinesStyle } from "@components-core/utils/css-utils";
 import { composeRefs } from "@radix-ui/react-compose-refs";
+import type { PropertyValueDescription } from "@abstractions/ComponentDefs";
 
 const TextVariantKeys = [
   "abbr", // use <abbr>
@@ -35,6 +36,7 @@ const TextVariantKeys = [
 ] as const;
 
 type TextVariant = (typeof TextVariantKeys)[number];
+type TextPropertyValueDescription = PropertyValueDescription & { value: TextVariant; description: string };
 
 const TextVariantElement: Record<TextVariant, TextVariantMapping> = {
   abbr: "abbr",
@@ -81,6 +83,33 @@ type TextVariantMapping =
   | "span"
   | "p"
   | "h6";
+
+export const variantOptionsMd: TextPropertyValueDescription[] = [
+  { value: "abbr", description: "Represents an abbreviation or acronym" },
+  { value: "caption", description: "Represents the caption (or title) of a table" },
+  { value: "cite", description: "Is used to mark up the title of a cited work" },
+  { value: "code", description: "Represents a line of code" },
+  { value: "codefence", description: "Handles the display of code blocks if combined with a `code` variant" },
+  { value: "deleted", description: "Represents text that has been deleted" },
+  { value: "em", description: "Marks text to stress emphasis" },
+  { value: "inserted", description: "Represents a range of text that has been added to a document" },
+  { value: "keyboard", description: "Represents a span of text denoting textual user input from a keyboard or voice input" },
+  { value: "marked", description: "Represents text which is marked or highlighted for reference or notation" },
+  { value: "mono", description: "Text using a mono style font family" },
+  { value: "paragraph", description: "Represents a paragraph" },
+  { value: "placeholder", description: "Text that is mostly used as the placeholder style in input controls" },
+  { value: "sample", description: "Represents sample (or quoted) output from a computer program" },
+  { value: "secondary", description: "Represents a bit dimmed secondary text" },
+  { value: "small", description: "Represents side-comments and small print" },
+  { value: "sub", description: "Specifies inline text as subscript" },
+  { value: "strong", description: "Contents have strong importance" },
+  { value: "subheading", description: "Indicates that the text is the subtitle in a heading" },
+  { value: "subtitle", description: "Indicates that the text is the subtitle of some other content" },
+  { value: "sup", description: "Specifies inline text as superscript" },
+  { value: "tableheading", description: "Indicates that the text is a table heading" },
+  { value: "title", description: "Indicates that the text is the title of some other content" },
+  { value: "var", description: "Represents the name of a variable in a mathematical expression" },
+] as const;
 
 const AbbreviationKeys = ["title"] as const;
 type Abbreviation = {
