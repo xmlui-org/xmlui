ce135979797a5d182f57dce876230b41c713d992
captaaint <56363291+captaaint@users.noreply.github.com>
2024-11-12 13:18:30 +0100

    wip

--- docs/public/resources/files/for-download/start.sh
+++ docs/public/resources/files/for-download/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Check the operating system type
+os_type=$(uname)
+
+if [ "$os_type" = "Linux" ]; then
+    # If Linux, restart the script with the /usr/bin/env shebang
+    exec /usr/bin/env bash "$0" "$@"
+elif [ "$os_type" = "Darwin" ]; then
+    # If macOS (Darwin), restart the script with the /bin/bash shebang
+    exec /bin/bash "$0" "$@"
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
 # Check if npx is available
 if command -v npx > /dev/null 2>&1; then
     # Run npx http-server -y -o
