bab38d2641c0de126581205dce3e3fb7857094ef
captaaint <56363291+captaaint@users.noreply.github.com>
2024-11-13 10:15:12 +0100

    wip

--- xmlui/src/components/Combobox/ComboboxNative2.tsx
+++ xmlui/src/components/Combobox/ComboboxNative2.tsx
@@ -18,6 +18,7 @@ import { CheckIcon } from "@components/Icon/CheckIcon";
 import { SelectContext, useSelectContextValue } from "@components/Select/SelectContext";
 import styles from "./Combobox2.module.scss";
 import type { CSSProperties, ReactNode } from "react";
+import { useRef, useState } from "react";
 import { useCallback, useEffect } from "react";
 import type { UpdateStateFn } from "@abstractions/RendererDefs";
 import { noop } from "@components-core/constants";
@@ -35,6 +36,8 @@ type Combobox2Props = {
   layout?: CSSProperties;
   emptyListTemplate?: ReactNode;
   optionRenderer?: (item: Option) => ReactNode;
+  onFocus?: () => void;
+  onBlur?: () => void;
 };
 
 function defaultRenderer(item: Option) {
@@ -51,11 +54,16 @@ export function Combobox2({
   enabled = true,
   updateState,
   onDidChange = noop,
+  onFocus = noop,
+  onBlur = noop,
   optionRenderer = defaultRenderer,
   emptyListTemplate,
 }: Combobox2Props) {
   const { options, selectContextValue } = useSelectContextValue();
+  const [referenceElement, setReferenceElement] = useState<HTMLElement | null>(null);
   const [open, setOpen] = React.useState(false);
+  const [width, setWidth] = useState(0);
+  const observer = useRef<ResizeObserver>();
 
   useEffect(() => {
     updateState({ value: initialValue });
@@ -69,21 +77,53 @@ export function Combobox2({
     [onDidChange, updateState],
   );
 
+  useEffect(() => {
+    const current = referenceElement as any;
+    // --- We are already observing old element
+    if (observer?.current && current) {
+      observer.current.unobserve(current);
+    }
+    observer.current = new ResizeObserver(() => setWidth((referenceElement as any).clientWidth));
+    if (current && observer.current) {
+      observer.current.observe(referenceElement as any);
+    }
+  }, [referenceElement]);
+
+  // --- Manage obtaining and losing the focus
+  const handleOnFocus = useCallback(() => {
+    onFocus?.();
+  }, [onFocus]);
+
+  const handleOnBlur = useCallback(() => {
+    onBlur?.();
+  }, [onBlur]);
+
   return (
     <SelectContext.Provider value={selectContextValue}>
       {children}
       <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild style={layout}>
-          <Button variant="outlined" aria-expanded={open} className={classnames(styles.comboboxButton, {
-            [styles.disabled]: !enabled,
-          })}>
+        <PopoverTrigger
+          asChild
+          style={layout}
+          ref={setReferenceElement}
+          id={id}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
+        >
+          <Button
+            variant="outlined"
+            aria-expanded={open}
+            className={classnames(styles.comboboxButton, {
+              [styles.disabled]: !enabled,
+            })}
+          >
             {value
               ? options.find((framework) => framework.value === value)?.label
               : placeholder || ""}
             <Icon name="chevrondown" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className={styles.popoverContent}>
+        <PopoverContent className={styles.popoverContent} style={{ width }}>
           <Command>
             <CommandInput placeholder="Search..." className={styles.commandInput} />
             <CommandList className={styles.commandList}>
