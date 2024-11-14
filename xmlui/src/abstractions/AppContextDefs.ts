ae398fe6c0380872e47045ada04d7a9cd64c5d81
Istvan Novak <dotneteer@hotmail.com>
2024-11-14 14:28:13 +0100

    refactor: check AppContext and its documentation (#419)

--- xmlui/src/abstractions/AppContextDefs.ts
+++ xmlui/src/abstractions/AppContextDefs.ts
@@ -1,6 +1,13 @@
 import type { To, NavigateOptions } from "react-router-dom";
 import type { QueryClient } from "@tanstack/react-query";
-import type { ToastOptions, Renderable, ValueOrFunction, DefaultToastOptions, Toast } from "react-hot-toast";
+
+import type {
+  ToastOptions,
+  Renderable,
+  ValueOrFunction,
+  DefaultToastOptions,
+  Toast,
+} from "react-hot-toast";
 
 import type { ActionFunction } from "./ActionDefs";
 /**
@@ -13,85 +20,209 @@ export type AppContextObject = {
    */
   [x: string]: unknown;
 
-  /**
-   * Navigate from the current location to the specified one using the given options.
-   * @param to New app location
-   * @param options Navigation options
-   */
-  readonly navigate: (to: To, options?: NavigateOptions) => void;
+  // ==============================================================================================
+  // Actions namespace
+
+  Actions: Record<string, ActionFunction>;
+
+  // ==============================================================================================
+  // App-Specific
 
   /**
-   * Display a confirmation dialog in the UI and retrieve the confirmation result.
-   * @param title Dialog title
-   * @param message Confirmation message
-   * @param actionMessage Optional action message
-   * @return True, if the user confirmed the action; otherwise, false.
+   * This property returns the context object of the API interceptor.
+   * @internal
    */
-  readonly confirm: (title: string, message?: string, actionMessage?: string) => Promise<boolean>;
+  apiInterceptorContext: IApiInterceptorContext;
 
   /**
-   * Get the information about the logged-in user. If null, no user is logged in.
+   * This property returns a hash object containing all application-global settings
+   * defined in the app's configuration file.
    */
-  readonly loggedInUser: LoggedInUserDto | null;
-  readonly setLoggedInUser: (loggedInUser: any) => void;
+  appGlobals?: Record<string, any>;
 
   /**
-   * The `QueryClient` object of the react-query library AppEngine uses for data fetching purposes
+   * Indicates that the application is running in debug-enabled mode.
+   * @internal
    */
-  readonly queryClient: QueryClient | null;
+  debugEnabled?: boolean;
 
   /**
-   * The base name of the router
+   * Indicates that components are decorated with test IDs used for e2e tests.
+   * @internal
    */
-  readonly routerBaseName: string;
+  decorateComponentsWithTestId?: boolean;
 
   /**
-   * Get an unformatted Date string that is JSON friendly.
-   * If no input param is set, use the current date time.
-   * @param date Potential Date string
+   * This property returns an object with some properties of the current environment.
    */
-  readonly isoDateString: (date?: string) => string;
+  environment: { isWindowFocused: boolean };
 
   /**
-   * A function that formats a date value to a string
+   * This property returns an object with information about the current media size.
+   */
+  mediaSize: MediaSize;
+
+  /**
+   * The `QueryClient` object of the react-query library XMLUI uses for data fetching purposes
+   * @internal
+   */
+  queryClient: QueryClient | null;
+
+  /**
+   * This property returns `true` if the app is a standalone XMLUI app; otherwise
+   * (for example, as part of a website), it returns `false`.
+   */
+  standalone?: boolean;
+
+  // ==============================================================================================
+  // Date Utilities
+
+  /**
+   * This function formats the specified value's date part into a local date string
+   * (according to the machine's local settings).
    * @param date Date to format
    */
-  readonly formatDate: (date: string | Date) => string | undefined;
+  formatDate: (date: string | Date) => string | undefined;
 
   /**
-   * A function that formats a date and time value to a string
+   * This function formats the specified value into a local
+   * date and time string (according to the machine's local settings).
    * @param date Date to format
    */
-  readonly formatDateTime: (date: any) => string | undefined;
+  formatDateTime: (date: any) => string | undefined;
 
   /**
-   * A function that formats a time value to a string
+   * This function formats the specified value's date part (without year)
+   * into a local date string (according to the machine's local settings).
    * @param date Date to format
    */
-  readonly formatTime: (date: any) => string | undefined;
+  formatDateWithoutYear: (date: string | Date) => string | undefined;
 
   /**
-   * A function that formats a file size number to a friendly string
+   * This function formats the specified value's time part into a local date
+   * string (according to the machine's local settings).
+   * @param date Date to format
+   */
+  formatTime: (date: any) => string | undefined;
+
+  /**
+   * This function formats the specified value's time part (without seconds)
+   * into a local date string (according to the machine's local settings).
+   * @param date Date to format
+   */
+  formatTimeWithoutSeconds: (date: string | Date) => string | undefined;
+
+  /**
+   * This function creates a date from the specified input value. If no input
+   *  is provided, it returns the current date and time.
+   */
+  getDate: (date?: string | number | Date) => Date;
+
+  /**
+   * This function calculates the difference between the current date and the
+   * provided one and returns it in a human-readable form, such as "1 month", "2 weeks", etc.
+   * @param date
+   * @param nowLabel
+   * @param time
+   * @returns
+   */
+  getDateUntilNow: (date?: string | number | Date, nowLabel?: string, time?: string) => string;
+
+  /**
+   * This function converts the input string into a date value and returns
+   * the ISO 8601 string representation of the date. It can pass dates between
+   * the UI and backend APIs in a standard format.
+   * @param date Date to format
+   */
+  isoDateString: (date?: string) => string;
+
+  /**
+   * This function checks if the specified date is today.
+   * @param date Date to check
+   */
+  isToday: (date: string | Date) => boolean;
+
+  /**
+   * This function checks if the specified date is tomorrow.
+   * @param date Date to check
+   */
+  isTomorrow: (date: string | Date) => boolean;
+
+  /**
+   * This function checks if the specified date is yesterday.
+   * @param date Date to check
+   */
+  isYesterday: (date: string | Date) => boolean;
+
+  /**
+   * This function checks the date value provided for some particular
+   * values and returns accordingly. Otherwise, returns it as `formatDate` would.
+   * @param date Date to format
+   */
+  smartFormatDate: (date?: string | number | Date) => string;
+
+  /**
+   * This function checks the date value provided for some particular values and
+   * returns accordingly. Otherwise, returns it as `formatDateTime` would.
+   * @param date Date to format
+   */
+  smartFormatDateTime: (date: string | Date) => string | undefined;
+
+  // ==============================================================================================
+  // File Utilities
+
+  /**
+   * This function returns the specified file size in a compact form, such as
+   * "112 B", "2.0 KiB", "23.4 KiB", "2.3 MiB", etc.
    * @param bytes Number of bytes to format
    */
-  readonly formatFileSizeInBytes: (bytes: number) => string | undefined;
+  formatFileSizeInBytes: (bytes: number) => string | undefined;
 
   /**
-   * A function that retrieves the extension of the specified file name
+   * This function returns the type of the specified file.
    * @param fileName Filename to check for extension
    */
-  readonly getFileExtension: (fileName: string) => string | undefined;
+  getFileExtension: (fileName: string) => string | undefined;
+
+  // ==============================================================================================
+  // Navigation Utilities
 
   /**
-   * Indicates that the root component represents a standalone application that runs without interacting with the
-   * storage engine.
+   * This function navigates to the specified `url`.
+   * @param url New app location
+   * @param options Navigation options
    */
-  readonly standalone?: boolean;
-  readonly decorateComponentsWithTestId?: boolean;
-  readonly debugEnabled?: boolean;
+  navigate: (url: To, options?: NavigateOptions) => void;
 
-  readonly signError: (error: Error | string) => void;
-  readonly toast: {
+  /**
+   * This property determines the base name used for the router.
+   */
+  routerBaseName: string;
+
+  // ==============================================================================================
+  // Notifications and Dialogs
+
+  /**
+   * Instructs the browser to display a dialog with an optional message, and to wait until the
+   * user either confirms or cancels the dialog. It returns a boolean indicating whether OK
+   * (`true`) or Cancel (`false`) was selected.
+   * @param title Dialog title
+   * @param message Confirmation message
+   * @param actionLabel Optional action label
+   * @return True, if the user confirmed the action; otherwise, false.
+   */
+  confirm: (title: string, message?: string, actionLabel?: string) => Promise<boolean>;
+
+  /**
+   * This method displays the specified `error` (error message) on the UI.
+   * @param error Error to display
+   */
+  signError(error: Error | string): void;
+
+  /**
+   * The toast service that displays messages in the UI.
+   */
+  toast: {
     (message: Message, opts?: ToastOptions): string;
     error: ToastHandler;
     success: ToastHandler;
@@ -106,32 +237,73 @@ export type AppContextObject = {
         success: ValueOrFunction<Renderable, T>;
         error: ValueOrFunction<Renderable, any>;
       },
-      opts?: DefaultToastOptions
+      opts?: DefaultToastOptions,
     ): Promise<T>;
   };
 
-  readonly activeThemeId: string;
-  readonly availableThemeIds: string[];
-  readonly setTheme: (newTheme: string) => void;
-  readonly appGlobals?: Record<string, any>;
+  // ==============================================================================================
+  // Theme-related
+
+  /**
+   * This property returns the ID of the currently active theme.
+   */
+  activeThemeId: string;
+
+  /**
+   * This property returns the tone of the currently active theme ("light" or "dark").
+   */
+  activeThemeTone: "light" | "dark";
+
+  /**
+   * This property returns an array of all available theme IDs.
+   */
+  availableThemeIds: string[];
+
+  /**
+   * This function sets the current theme to the one with the specified `themeId`.
+   * @param themId New theme to set
+   */
+  setTheme: (themId: string) => void;
+
+  /**
+   * This function sets the current theme tone to the specified `tone` value ("light" or "dark").
+   * @param newTone New tone to set
+   */
+  setThemeTone: (newTone: "light" | "dark") => void;
+
+  /**
+   * This function toggles the current theme tone from "light" to "dark" or vice versa.
+   */
+  toggleThemeTone: () => void;
+
+  // ==============================================================================================
+  // Users
+
+  /**
+   * This property gets the information about the logged-in user. If `null`, no user is
+   * logged in. The user information may have any value; the app must be able to leverage
+   * this information.
+   */
+  loggedInUser: LoggedInUserDto | null;
+
+  /**
+   * This function sets the information about the logged-in user. The user information
+   * may have any value; the app must be able to leverage this information.
+   * @param loggedInUser Logged-in user information
+   */
+  setLoggedInUser: (loggedInUser: any) => void;
+
   readonly resources?: Record<string, string>;
 
-  readonly apiInterceptorContext: IApiInterceptorContext;
-  readonly formatTimeWithoutSeconds: (date: string | Date) => string | undefined;
-  readonly formatDateWithoutYear: (date: string | Date) => string | undefined;
-  readonly smartFormatDateTime: (date: string | Date) => string | undefined;
-  readonly smartFormatDate: (date: string | Date) => string | undefined;
+  // ==============================================================================================
+  // Various Utilities
+
   readonly capitalize: (s?: string) => string;
   readonly pluralize: (number: number, singular: string, plural: string) => string;
   readonly delay: (timeInMs: number, callback?: any) => Promise<void>;
-  readonly getDate: () => Date;
-  readonly getDateUntilNow: (date?: string | number | Date, nowLabel?: string, time?: string) => string;
-  readonly Actions: Record<string, ActionFunction>;
   readonly Transforms: Record<string, any>;
   readonly DateUtils: Record<string, any>;
   readonly embed: { isInIFrame: boolean };
-  readonly environment: { isWindowFocused: boolean };
-  readonly mediaSize: MediaSize;
 };
 
 export type MediaBreakpointType = "xs" | "sm" | "md" | "lg" | "xl" | "xxl";
@@ -147,7 +319,7 @@ export type MediaSize = {
   largeScreen: boolean;
   size: MediaBreakpointType;
   sizeIndex: number;
-}
+};
 
 export type LoggedInUserDto = {
   id: number;
