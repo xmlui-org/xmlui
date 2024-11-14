e760fb01988a803494fe7fbef1a7b0c35f67eb98
Istvan Novak <dotneteer@hotmail.com>
2024-11-14 14:27:49 +0100

    refactor: check AppContext and its documentation

--- xmlui/src/components-core/appContext/misc-utils.ts
+++ xmlui/src/components-core/appContext/misc-utils.ts
@@ -1,15 +1,11 @@
 import { capitalize, defaultTo } from "lodash-es";
 
 import {
-  formatFileSizeInBytes,
-  getFileExtension,
   pluralize,
   toHashObject,
 } from "@components-core/utils/misc";
 
 export const miscellaneousUtils = {
-  formatFileSizeInBytes,
-  getFileExtension,
   capitalize,
   pluralize,
   defaultTo,
