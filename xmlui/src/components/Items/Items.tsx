c54dbebea3d102c31195ca7dc03885a2d134b30e
Istvan Novak <dotneteer@hotmail.com>
2024-11-18 07:18:06 +0100

    docs: update the structure of generated docs

--- xmlui/src/components/Items/Items.tsx
+++ xmlui/src/components/Items/Items.tsx
@@ -18,30 +18,34 @@ export const ItemsMd = createMetadata({
     reverse: d(`This property reverses the order in which data is mapped to template components.`),
     itemTemplate: dComponent("The component template to display a single item"),
   },
+  contextVars: {
+    $item: dComponent(
+      "This value represents the current iteration item while the component renders its children.",
+    ),
+    $itemIndex: dComponent("This integer value represents the current iteration index (zero-based) while rendering children."),
+    $isFirst: dComponent("This boolean value indicates if the component renders its first item."),
+    $isLast: dComponent("This boolean value indicates if the component renders its last item."),
+  },
   opaque: true,
 });
 
-export const itemsComponentRenderer = createComponentRenderer(
-  COMP,
-  ItemsMd,
-  (rendererContext) => {
-    const { node, renderChild, extractValue, layoutContext } = rendererContext;
-    return (
-      <Items
-        items={extractValue(node.props.items) || extractValue(node.props.data)}
-        reverse={extractValue(node.props.reverse)}
-        renderItem={(contextVars, key) => {
-          return (
-            <MemoizedItem
-              key={key}
-              contextVars={contextVars}
-              node={node.children || (node.props.itemTemplate as any)}
-              renderChild={renderChild}
-              layoutContext={layoutContext}
-            />
-          );
-        }}
-      />
-    );
-  },
-);
+export const itemsComponentRenderer = createComponentRenderer(COMP, ItemsMd, (rendererContext) => {
+  const { node, renderChild, extractValue, layoutContext } = rendererContext;
+  return (
+    <Items
+      items={extractValue(node.props.items) || extractValue(node.props.data)}
+      reverse={extractValue(node.props.reverse)}
+      renderItem={(contextVars, key) => {
+        return (
+          <MemoizedItem
+            key={key}
+            contextVars={contextVars}
+            node={node.children || (node.props.itemTemplate as any)}
+            renderChild={renderChild}
+            layoutContext={layoutContext}
+          />
+        );
+      }}
+    />
+  );
+});
