1c9ccf6b8d32121e9a77830a9343ab1d6fe96eab
Istvan Novak <dotneteer@hotmail.com>
2024-12-03 10:25:45 +0100

    feat: improve the Markdown component (#478)

--- xmlui/src/components/Markdown/Markdown.tsx
+++ xmlui/src/components/Markdown/Markdown.tsx
@@ -1,4 +1,4 @@
-import { createMetadata } from "@abstractions/ComponentDefs";
+import { createMetadata, d } from "@abstractions/ComponentDefs";
 import { createComponentRenderer } from "@components-core/renderers";
 import styles from "./Markdown.module.scss";
 import { parseScssVar } from "@components-core/theming/themeVars";
@@ -9,6 +9,18 @@ const COMP = "Markdown";
 export const MarkdownMd = createMetadata({
   description: `\`${COMP}\` displays plain text styled using markdown syntax.`,
   themeVars: parseScssVar(styles.themeVars),
+  props: {
+    content: d("This property sets the markdown content to display."),
+    removeIndents: d(
+      "This boolean property specifies whether leading indents should be " +
+        "removed from the markdown content. If set to `true`, the shortest " +
+        "indent found at the start of the content lines is removed from the " +
+        "beginning of every line.",
+      null,
+      "boolean",
+      false,
+    ),
+  },
   defaultThemeVars: {
     "color-border-HorizontalRule": "$color-border",
     "thickness-border-HorizontalRule": "1px",
@@ -31,7 +43,15 @@ export const MarkdownMd = createMetadata({
 export const markdownComponentRenderer = createComponentRenderer(
   COMP,
   MarkdownMd,
-  ({ node, renderChild }) => {
-    return <Markdown>{renderChild(node.children)}</Markdown>;
+  ({ node, extractValue, renderChild, layoutCss }) => {
+    const content = extractValue.asString(node.props.content);
+    return (
+      <Markdown
+        layout={layoutCss}
+        removeIndents={extractValue.asOptionalBoolean(node.props.removeIndents, false)}
+      >
+        {content || renderChild(node.children)}
+      </Markdown>
+    );
   },
 );
