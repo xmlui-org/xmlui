ae124a422914851cfb9877619c10f69d00be9185
Marcell Kiss <52428841+marcello-tk@users.noreply.github.com>
2024-12-04 10:45:20 +0000

    test: add Button label assertion, icon resources, expand asOptionalBoolean (#491)

--- xmlui/src/components/Button/Button.spec.ts
+++ xmlui/src/components/Button/Button.spec.ts
@@ -1,17 +1,74 @@
-import { expect, ComponentDriver, createTestWithDriver } from "@testing/fixtures";
+import type { Locator } from "@playwright/test";
+import { expect as fixtureExpect, ComponentDriver, createTestWithDriver } from "@testing/fixtures";
 
 // --- Setup
 
-class ButtonDriver extends ComponentDriver {
+const expect = fixtureExpect.extend({
+  /**
+   * Asserts whether the Button component has the expected label either through the `label` property
+   * or as a text node child.
+   *
+   * **Usage**
+   *
+   * ```js
+   * const driver = await createDriver(`<Button label="hello" />`);
+   * await expect(driver.button).toHaveLabel("hello"); // true
+   * ```
+   *
+   * @param expected Expected string label
+   */
+  async toHaveLabel(locator: Locator, expected: string) {
+    const assertionName = "toHaveLabel";
+    let pass = false;
 
-  get button () {
+    const label = await locator.evaluate((element) =>
+      [...element.childNodes]
+        .filter(e => e.nodeType === Node.TEXT_NODE && e.textContent.trim())
+        .map(e => e.textContent.trim())
+        ?.[0]);
+
+    if (label === expected) {
+      pass = true;
+    }
+
+    const message = pass
+      ? () =>
+          this.utils.matcherHint(assertionName, locator, expected, { isNot: this.isNot }) +
+          "\n\n" +
+          `Expected: ${this.isNot ? "not" : ""}${this.utils.printExpected(expected)}\n` +
+          `Received: ${this.utils.printReceived(label)}`
+      : () =>
+          this.utils.matcherHint(assertionName, locator, expected, { isNot: this.isNot }) +
+          "\n\n" +
+          `Expected: ${this.utils.printExpected(expected)}\n` +
+          `Received: ${this.utils.printReceived(label)}`;
+
+    return {
+      message,
+      pass,
+      name: assertionName,
+      expected,
+      actual: undefined,
+    };
+  },
+});
+
+class ButtonDriver extends ComponentDriver {
+  get button() {
     return this.locator;
   }
 
-  get buttonIcon () {
+  get buttonIcon() {
     return this.locator.locator("svg").or(this.locator.locator("img"));
   }
 
+  /* islayoutHorizontal = async () => {
+    const isFlex = await this.button.evaluate(
+      (element) => element.style.display === "flex" || element.style.display === "inline-flex",
+    );
+    const iconIsInline = await this.buttonIcon.evaluate((element) => (element.style.display = "inline-block"));
+    return isFlex || iconIsInline;
+  }; */
 }
 
 const test = createTestWithDriver(ButtonDriver);
@@ -61,46 +118,50 @@ test("renders without label, icon or children", async ({ createDriver }) => {
   });
 });
 
-test("ignores label if children present", async ({ createDriver }) => {
-  const driver = await createDriver(`<Button label="hello">world</Button>`);
-  await expect(driver.button).toHaveText("world");
+test("text node as children are same as setting label", async ({ createDriver }) => {
+  const driver = await createDriver(`<Button>hello</Button>`);
+  await expect(driver.button).toHaveLabel("hello");
 });
 
-// Should this be different compared to the regular label test?
+test("ignores label property if children present", async ({ createDriver }) => {
+  const driver = await createDriver(`<Button label="hello">world</Button>`);
+  await expect(driver.button).toHaveLabel("world");
+});
+
 test.skip("renders XMLUI Text component as child", async ({ createDriver }) => {
   const driver = await createDriver(`<Button label="hello"><Text>world</Text></Button>`);
-  await expect(driver.button).toHaveText("world");
+  await expect(driver.button).not.toHaveLabel("hello");
 });
 
 // Not elaborated yet - need to get the children reliably
 test.skip("renders XMLUI Complex component as child", async ({ createDriver }) => {
-  const driver = await createDriver(`<Button label="hello"><Card title="Button">Content</Card></Button>`);
+  const driver = await createDriver(
+    `<Button label="hello"><Card title="Button">Content</Card></Button>`,
+  );
   await expect(driver.button).toHaveText("world");
 });
 
 // --- --- icon
 
 test("can render icon", async ({ createDriver }) => {
-  // const driver = await createDriver(`<Button icon="test" />`, { "icon.test": "resources/xmlui-logo.svg" });
-  const driver = await createDriver(`<Button icon="trash" />`);
-  await expect(driver.buttonIcon).toBeVisible();
-});
-
-test.skip("renders correct icon", async ({ createDriver }) => {
-  // const driver = await createDriver(`<Fragment><Button icon="test" /><Icon name="test" /></Fragment>`,
-  // { "icon.test": "resources/xmlui-logo.svg" });
-  const driver = await createDriver(`<Button icon="trash" />`);
+  const driver = await createDriver(`<Button icon="test" />`, {
+    resources: { "icon.test": "resources/bell.svg" },
+  });
   await expect(driver.buttonIcon).toBeVisible();
 });
 
 test("renders icon and label", async ({ createDriver }) => {
-  const driver = await createDriver(`<Button icon="trash" label="hello" />`);
+  const driver = await createDriver(`<Button icon="test" label="hello" />`, {
+    resources: { "icon.test": "resources/bell.svg" },
+  });
   await expect(driver.button).toHaveText("hello");
   await expect(driver.buttonIcon).toBeVisible();
 });
 
 test("renders icon if children present", async ({ createDriver }) => {
-  const driver = await createDriver(`<Button icon="trash">Hello World</Button>`);
+  const driver = await createDriver(`<Button icon="test">Hello World</Button>`, {
+    resources: { "icon.test": "resources/bell.svg" },
+  });
   await expect(driver.buttonIcon).toBeVisible();
 });
 
@@ -119,10 +180,10 @@ test("renders icon if children present", async ({ createDriver }) => {
   });
 });
 
-test.skip("renders if icon is not found and label is present", async ({ createDriver }) => {
-  /* const driver = await createDriver(`<Button icon="___" label="hello" />`);
+test("renders if icon is not found and label is present", async ({ createDriver }) => {
+  const driver = await createDriver(`<Button icon="_" label="hello" />`);
   await expect(driver.buttonIcon).not.toBeAttached();
-  await expect(driver.button).toHaveText("hello"); */
+  await expect(driver.button).toHaveText("hello");
 });
 
 // --- --- iconPosition
@@ -149,7 +210,9 @@ test.skip("end position appears at right of label", async ({ createDriver }) => 
 
 test.skip("left position appears left of label in ltr (no label)", async ({ createDriver }) => {});
 
-test.skip("right position appears right of label in ltr (no label)", async ({ createDriver }) => {});
+test.skip("right position appears right of label in ltr (no label)", async ({
+  createDriver,
+}) => {});
 
 test.skip("start position appears at left of label (no label)", async ({ createDriver }) => {});
 
@@ -211,9 +274,13 @@ test("focuses component if autoFocus is set", async ({ createDriver }) => {
 // import from abstractions: buttonThemeMd
 ["solid", "outlined", "ghost"].forEach((variant) => {
   ["primary", "secondary", "attention"].forEach((themeColor) => {
-    test.skip(`themeColor "${themeColor}" is applied for variant "${variant}"`, async ({ createDriver }) => {});
+    test.skip(`themeColor "${themeColor}" is applied for variant "${variant}"`, async ({
+      createDriver,
+    }) => {});
     ["disabled", "hover", "active", "focused"].forEach((state) => {
-      test.skip(`${state} state for themeColor "${themeColor}" is applied for variant "${variant}"`, async ({ createDriver }) => {});
+      test.skip(`${state} state for themeColor "${themeColor}" is applied for variant "${variant}"`, async ({
+        createDriver,
+      }) => {});
     });
   });
 });
@@ -241,17 +308,6 @@ test("click event fires", async ({ createDriver }) => {
   const driver = await createDriver(`<Button onClick="testState = true" />`);
   await driver.click();
   await expect.poll(driver.testState).toEqual(true);
-});
-
-test("click event does not fire if disabled", async ({ createDriver }) => {
-  const driver = await createDriver(`<Button enabled="false" onClick="testState = true" />`);
-  try {
-    await driver.click({ timeout: 500 });
-  } catch (e) {
-    // This click event is deliberately ignored
-  }
-  // testState remains unchanged
-  await expect.poll(driver.testState).toEqual(null);
 });
 
 // --- --- gotFocus
@@ -300,3 +356,26 @@ test("lostFocus event does not fire if disabled", async ({ createDriver }) => {
   await expect(driver.button).not.toBeFocused();
   await expect.poll(driver.testState).toEqual(null);
 });
+
+// --- Should be added to tests regarding the framework loading mechanism:
+/* 
+test("can render correct icon", async ({ createDriver }) => {
+  // 1. Define the icon resource we wish to load
+  // 2. Provide the XLMUI app ecosystem with the resource
+  // 3. Fetch the icon ourselves
+  // 4. Compare both icons 
+  
+  const testIcon = `
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell"
+       width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+       stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+    <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+  </svg>
+  `;
+
+  const driver = await createDriver(`<Button icon="test" />`, { resources: { "icon.test": "resources/bell.svg" } });
+  await expect(driver.buttonIcon).toBeVisible();
+});
+*/
