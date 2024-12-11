b6c1689d7a283a36d5a9c35c7cc71718da019643
marcello-tk <awesomedevguynull@gmail.com>
2024-12-11 15:08:08 +0000

    test: add NumberBox cases

--- xmlui/src/components/Items/Items.spec.ts
+++ xmlui/src/components/Items/Items.spec.ts
@@ -9,19 +9,33 @@ const test = createTestWithDriver(ItemsDriver);
 
 // --- Testing
 
-test.skip("Items does not render htmlon its own", async ({ createDriver }) => {});
+test.skip("Items does not render html on its own", async ({ createDriver }) => {
+  // Place empty Items in the app and check that it does not render html
+});
 
-test.skip("Items only renders children", async ({ createDriver }) => {});
+test.skip("data property renders children", async ({ createDriver }) => {
+  // Setting the data property renders children 
+});
 
-test.skip("data property renders children", async ({ createDriver }) => {});
+test.skip("customize item template for children", async ({ createDriver }) => {
+  // Set itemTemplate="..."
+});
 
-test.skip("order of children is reversed", async ({ createDriver }) => {});
+test.skip("data property only renders children if itemTemplate is set", async ({ createDriver }) => {
+  // Setting the data property renders children 
+});
 
-test.skip("customize item template for children", async ({ createDriver }) => {});
+test.skip("Items only renders children", async ({ createDriver }) => {
+  // When children are provided, Items does not render extra containers in html
+});
 
-test.skip("$item accesses the shape of data", async ({ createDriver }) => {});
+test.skip("order of children is reversed", async ({ createDriver }) => {
+  // Set reverse="true" and check that the order of children is reversed
+});
 
-test.skip("$itemIndex accesses the index of a data item", async ({ createDriver }) => {});
+test.skip("$item can access the shape of data", async ({ createDriver }) => {});
+
+test.skip("$itemIndex provides the index of a data item", async ({ createDriver }) => {});
 
 test.skip("$isFirst tests if $item is first", async ({ createDriver }) => {});
 
---
+++ xmlui/src/components/NumberBox/NumberBox.spec.ts
@@ -0,0 +1,238 @@
+import { labelPositionValues, validationStatusValues } from "@components/abstractions";
+import { ComponentDriver, createTestWithDriver } from "@testing/fixtures";
+
+// --- Setup
+
+class NumberBoxDriver extends ComponentDriver {}
+
+const test = createTestWithDriver(NumberBoxDriver);
+
+// --- Testing
+
+// --- props
+
+// --- --- placeholder
+
+test.skip("placeholder appears if input field is empty", async ({ createDriver }) => {});
+
+test.skip("placeholder is hidden if input field is filled", async ({ createDriver }) => {});
+
+// --- --- initialValue
+
+// correct types: number, undefined, null, string that resolves to a VALID number
+[
+  { label: "number", value: 1 },
+  { label: "undefined", value: undefined },
+  { label: "null", value: null },
+  { label: "empty string", value: "" },
+  { label: "string that resolves to number", value: "1" },
+].forEach(({ label, value }) => {
+  test.skip(`setting initialValue with ${label} sets value of field`, async ({ createDriver }) => {});
+});
+
+[
+  { label: "boolean", value: true },
+  { label: "non-number string", value: "asdasd" },
+  { label: "empty array", value: [] },
+  { label: "array", value: [1, 2, 3] },
+  { label: "empty object", value: {} },
+  { label: "object", value: { a: 1, b: "hi" } },
+  { label: "function", value: () => {} },
+  { label: "NaN", value: NaN },   // <- Not sure about this one
+].forEach(({ label, value }) => {
+  test.skip(`setting initialValue with ${label} throws error`, async ({ createDriver }) => {});
+})
+
+test.skip("if initialValue is too large, cap actual value to MAX_VALUE constant", async ({ createDriver }) => {
+  // initialValue="100000000000000000000000000000000000"
+});
+
+test.skip("if initialValue is too small, cap actual value to -MAX_VALUE constant", async ({ createDriver }) => {
+  // initialValue="-100000000000000000000000000000000000"
+});
+
+// --- --- label
+
+test.skip("label is rendered if provided", async ({ createDriver }) => {});
+
+test.skip("empty string label is not rendered", async ({ createDriver }) => {});
+
+test.skip("clicking on the label focuses input field", async ({ createDriver }) => {});
+
+// --- --- labelPosition
+
+labelPositionValues.forEach((pos) => {
+  test.skip(`label position ${pos} is applied for the input field and associated label`, async ({ createDriver }) => {});  
+});
+
+// --- --- labelWidth TODO?
+
+// --- --- labelBreak TODO?
+
+// --- --- maxLength
+
+test.skip("maxLength caps the length of the input", async ({ createDriver }) => {});
+
+// --- --- autoFocus
+
+test.skip("autoFocus sets focus on the input field on page load", async ({ createDriver }) => {});
+
+// --- --- required
+
+test.skip("making field required shows a visual indicator", async ({ createDriver }) => {});
+
+// --- --- readOnly
+
+test.skip("readOnly disables input field", async ({ createDriver }) => {});
+
+test.skip("readOnly lets user copy from input field", async ({ createDriver }) => {});
+
+// --- --- enabled
+
+test.skip("disabled input field stops user interaction for field", async ({ createDriver }) => {
+  // try to click input field
+});
+
+test.skip("disabled input field stops user interaction for spinbox", async ({ createDriver }) => {
+  // try to click spinbox
+});
+
+// --- --- validationStatus
+
+const validationStatuses = validationStatusValues.filter((v) => v !== "none");
+validationStatuses.forEach((status) => {
+  test.skip(`validation status ${status} is applied correctly`, async ({ createDriver }) => {
+    // border color matches the one specified in current theme
+  });
+});
+
+// --- --- startText
+
+test.skip("startText is rendered at the start of the field", async ({ createDriver }) => {
+  // check text content and position
+});
+
+// --- --- startIcon
+
+test.skip("startIcon is rendered at the start of the field", async ({ createDriver }) => {
+  // check rendered icon (similar to Icon test with loading resource) and position
+});
+
+// --- --- endText
+
+test.skip("endText is rendered at the end of the field", async ({ createDriver }) => {
+  // check text content and position
+});
+
+// --- --- endIcon
+
+test.skip("endIcon is rendered at the end of the field", async ({ createDriver }) => {
+  // check rendered icon (similar to Icon test with loading resource) and position
+});
+
+// --- --- hasSpinBox
+
+test.skip("renders spinbox if set to true", async ({ createDriver }) => {});
+
+test.skip("does not render spinbox if set to false", async ({ createDriver }) => {});
+
+test.skip("clicking spinbox up-arrow adds default step value to input", async ({ createDriver }) => {});
+
+test.skip("clicking spinbox down-arrow subtracts default step value from input", async ({ createDriver }) => {});
+
+test.skip("clicking spinbox up-arrow that would overflow max value does not add value", async ({ createDriver }) => {});
+
+test.skip("clicking spinbox down-arrow that would underflow min value does not subtract value", async ({ createDriver }) => {});
+
+// --- --- step
+
+test.skip("setting valid integer step adds that value to input", async ({ createDriver }) => {});
+
+[
+  { label: "boolean", value: true },
+  { label: "non-number string", value: "asdasd" },
+  { label: "empty array", value: [] },
+  { label: "array", value: [1, 2, 3] },
+  { label: "empty object", value: {} },
+  { label: "object", value: { a: 1, b: "hi" } },
+  { label: "function", value: () => {} },
+  { label: "NaN", value: NaN },
+  { label: "null", value: null },
+  { label: "undefined", value: undefined },
+  { label: "empty string", value: "" },
+  { label: "string resolves to number", value: "1" },
+  { label: "too large string number", value: "1000000000000000000000000000" },
+  { label: "too small string number", value: "-1000000000000000000000000000" },
+  { label: "float", value: 1.2 },
+  { label: "negative float", value: -1.2 },
+  { label: "string resolves to float", value: "1.2" },
+  { label: "string resolves to negative float", value: "-1.2" },
+  { label: "string resolves to number with e", value: "1e10" },
+].forEach(({ label, value }) => {
+  test.skip(`${label} is ignored and default step value is used`, async ({ createDriver }) => {});
+});
+
+// --- --- integersOnly
+
+test.skip("integersOnly limits input to integers", async ({ createDriver }) => {});
+
+// --- --- zeroOrPositive
+
+test.skip("zeroOrPositive limits input to non-negative numbers and zero", async ({ createDriver }) => {});
+
+test.skip("user cannot copy a negative number", async ({ createDriver }) => {});
+
+test.skip("down button on spinbox does nothing when result would be negative", async ({ createDriver }) => {});
+
+// --- --- minValue
+
+test.skip("minValue limits input to numbers greater than or equal to minValue", async ({ createDriver }) => {});
+
+test.skip("user cannot copy a number less than minValue", async ({ createDriver }) => {});
+
+// --- --- maxValue
+
+test.skip("maxValue limits input to numbers less than or equal to maxValue", async ({ createDriver }) => {});
+
+test.skip("user cannot copy a number greater than maxValue", async ({ createDriver }) => {});
+
+// --- events
+
+// --- --- onDidChange
+
+test.skip("onDidChange is called on input change", async ({ createDriver }) => {});
+
+test.skip("onDidChange is not called if field is disabled", async ({ createDriver }) => {});
+
+// --- --- gotFocus
+
+test.skip("gotFocus event fires on focusing the field", async ({ createDriver }) => {});
+
+test.skip("onFocus is not called if field is disabled", async ({ createDriver }) => {});
+
+// --- --- lostFocus
+
+test.skip("lostFocus event fires on blurring the field", async ({ createDriver }) => {});
+
+test.skip("lostFocus is not called if field is disabled", async ({ createDriver }) => {});
+
+// --- api
+
+// --- --- focus
+
+test.skip("focus() focuses the field", async ({ createDriver }) => {});
+
+test.skip("focus() does nothing if field is disabled", async ({ createDriver }) => {});
+
+// --- --- value
+
+test.skip("value returns current input value", async ({ createDriver }) => {});
+
+// --- --- setValue
+
+test.skip("setValue updates input value", async ({ createDriver }) => {});
+
+test.skip("setValue does not update input if field is disabled", async ({ createDriver }) => {});
+
+// TODO: do an array for invalid input values?
+test.skip("setValue does not update input if value is invalid", async ({ createDriver }) => {});
