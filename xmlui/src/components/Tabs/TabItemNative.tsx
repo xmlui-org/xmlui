e527ce0bd4a67159eb97371efc13787f01be7dd1
captaaint <56363291+captaaint@users.noreply.github.com>
2025-01-22 19:16:26 +0100

    wip

--- xmlui/src/components/Tabs/TabItemNative.tsx
+++ xmlui/src/components/Tabs/TabItemNative.tsx
@@ -1,20 +1,32 @@
-import { useEffect, useId } from "react";
+import { ForwardedRef, forwardRef, useEffect, useId } from "react";
 import type { Tab } from "@components/abstractions";
+import styles from "@components/Tabs/Tabs.module.scss";
+import { Content } from "@radix-ui/react-tabs";
 import { useTabContext } from "@components/Tabs/TabContext";
 
-export function TabItemComponent(props: Tab) {
+export const TabItemComponent = forwardRef(function TabItemComponent(
+  props: Tab,
+  forwardedRef: ForwardedRef<HTMLDivElement>,
+) {
   const id = useId();
   const { register, unRegister } = useTabContext();
+
   useEffect(() => {
     register({
       ...props,
       id,
     });
   }, [id, props, register]);
+
   useEffect(() => {
     return () => {
       unRegister(id);
     };
   }, [id, unRegister]);
-  return null;
-}
+
+  return (
+    <Content key={id} value={id} className={styles.tabsContent} ref={forwardedRef}>
+      {props.content}
+    </Content>
+  );
+});
