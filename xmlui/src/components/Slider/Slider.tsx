b704561a7a239740551b37ffa1bae9b6d90a80a4
captaaint <56363291+captaaint@users.noreply.github.com>
2025-02-11 11:18:58 +0100

    wip

--- xmlui/src/components/Slider/Slider.tsx
+++ xmlui/src/components/Slider/Slider.tsx
@@ -4,26 +4,37 @@ import { createMetadata, d } from "../../abstractions/ComponentDefs";
 import { createComponentRenderer } from "../../components-core/renderers";
 import { parseScssVar } from "../../components-core/theming/themeVars";
 import {
+  dAutoFocus,
   dDidChange,
+  dEnabled,
   dFocus,
   dGotFocus,
+  dInitialValue,
+  dLabel,
+  dLabelBreak,
+  dLabelPosition,
+  dLabelWidth,
   dLostFocus,
-  dSetValueApi,
+  dReadonly,
+  dRequired,
+  dSetValueApi, dValidationStatus,
   dValue,
 } from "../metadata-helpers";
 import { Slider } from "./SliderNative";
 
 const COMP = "Slider";
 
-// This component must be focusable and keyboard accessible.
-// Add this component to the form infrastructure.
-
 export const SliderMd = createMetadata({
   status: "in progress",
   description:
-    `(**NOT IMPLEMENTED YET**) The \`${COMP}\` component allows you to select a numeric value ` +
+    `The \`${COMP}\` component allows you to select a numeric value ` +
     `between a range specified by minimum and maximum values.`,
   props: {
+    initialValue: dInitialValue(),
+    label: dLabel(),
+    labelPosition: dLabelPosition("top"),
+    labelWidth: dLabelWidth(COMP),
+    labelBreak: dLabelBreak(COMP),
     minValue: d(
       `This property specifies the minimum value of the allowed input range.`,
       null,
@@ -36,29 +47,22 @@ export const SliderMd = createMetadata({
       "number",
       10,
     ),
-    rangeHighlight: d(
-      `This property indicates if you want to highlight a specific part of the input range.`,
-      [
-        { value: "none", description: "No highlight (default)" },
-        {
-          value: "lower",
-          description:
-            "Highlight the lower part of the range (from the minimum to the current slider value)",
-        },
-        {
-          value: "upper",
-          description:
-            "Highlight the upper part of the range (from the current slider value to the maximum)",
-        },
-      ],
-      "string",
-      "none",
+    step: d(
+      `This property defines the increment value for the slider, determining the allowed intervals between selectable values.`,
     ),
+    minStepsBetweenThumbs: d(
+      `This property sets the minimum number of steps required between multiple thumbs on the slider, ensuring they maintain a specified distance.`,
+    ),
+    enabled: dEnabled(),
+    autoFocus: dAutoFocus(),
+    required: dRequired(),
+    readOnly: dReadonly(),
+    validationStatus: dValidationStatus(),
   },
   events: {
     didChange: dDidChange(COMP),
-    getFocus: dGotFocus(COMP),
-    dostFocus: dLostFocus(COMP),
+    gotFocus: dGotFocus(COMP),
+    lostFocus: dLostFocus(COMP),
   },
   apis: {
     focus: dFocus(COMP),
@@ -67,15 +71,61 @@ export const SliderMd = createMetadata({
   },
   themeVars: parseScssVar(styles.themeVars),
   defaultThemeVars: {
-    light: {},
-    dark: {},
+    [`color-bg-track-${COMP}`]: "$color-primary",
+    [`color-bg-range-${COMP}`]: "$color-surface-500",
+    [`thickness-border-thumb-${COMP}`]: "2px",
+    [`style-border-thumb-${COMP}`]: "solid",
+    [`color-border-thumb-${COMP}`]: "$color-surface-200",
+    [`color-bg-thumb-${COMP}`]: "$color-surface-200",
+    [`shadow-thumb-${COMP}`]: "$shadow-md",
+
+    light: {
+      [`color-bg-track-${COMP}--disabled`]: "$color-surface-300",
+      [`color-bg-range-${COMP}--disabled`]: "$color-surface-400",
+    },
+    dark: {
+      [`color-bg-track-${COMP}--disabled`]: "$color-surface-600",
+      [`color-bg-range-${COMP}--disabled`]: "$color-surface-800",
+    },
   },
 });
 
 export const sliderComponentRenderer = createComponentRenderer(
   COMP,
   SliderMd,
-  ({ node, extractValue, lookupEventHandler, layoutCss }) => {
-    return <Slider style={layoutCss} />;
+  ({
+    node,
+    extractValue,
+    lookupEventHandler,
+    layoutCss,
+    updateState,
+    state,
+    registerComponentApi,
+  }) => {
+    return (
+      <Slider
+        validationStatus={extractValue(node.props.validationStatus)}
+        minStepsBetweenThumbs={extractValue(node.props?.minStepsBetweenThumbs)}
+        value={state.value}
+        initialValue={extractValue(node.props.initialValue)}
+        updateState={updateState}
+        onDidChange={lookupEventHandler("didChange")}
+        onFocus={lookupEventHandler("gotFocus")}
+        onBlur={lookupEventHandler("lostFocus")}
+        registerComponentApi={registerComponentApi}
+        style={layoutCss}
+        step={extractValue(node.props?.step)}
+        min={extractValue(node.props?.minValue)}
+        max={extractValue(node.props?.maxValue)}
+        enabled={extractValue.asOptionalBoolean(node.props?.enabled)}
+        autoFocus={extractValue.asOptionalBoolean(node.props.autoFocus)}
+        readOnly={extractValue.asOptionalBoolean(node.props.readOnly)}
+        label={extractValue.asOptionalString(node.props.label)}
+        labelPosition={extractValue(node.props.labelPosition)}
+        labelWidth={extractValue.asOptionalString(node.props.labelWidth)}
+        labelBreak={extractValue.asOptionalBoolean(node.props.labelBreak)}
+        required={extractValue.asOptionalBoolean(node.props.required)}
+      />
+    );
   },
 );
