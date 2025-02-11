d86d8ce3bbcab6ba64eb382126c82fae4a7e12ac
gergelyilles <illes.gergo@gmail.com>
2025-02-11 10:36:37 +0100

    using virtua for list virtualization

--- xmlui/bin/preview.ts
+++ xmlui/bin/preview.ts
@@ -1,9 +1,27 @@
 import { preview as vitePreview } from "vite";
 import { getViteConfig } from "./viteConfig";
 
-export const preview = async () => {
+export const preview = async ({proxy}) => {
   try {
-    const server = await vitePreview(getViteConfig());
+    let proxyDef;
+    if (proxy) {
+      const splitProxy = proxy.split("->");
+      if (splitProxy.length !== 2) {
+        console.error("Invalid proxy definition. Example: /api->http://localhost:3000");
+      } else {
+        proxyDef = {
+          [splitProxy[0]]: {
+            target: splitProxy[1],
+            changeOrigin: true,
+          },
+        };
+      }
+    }
+    const server = await vitePreview({ ...getViteConfig(),
+      server: {
+        proxy: proxyDef,
+      },
+    });
     
     if (!server.httpServer) {
       throw new Error("HTTP server not available");
