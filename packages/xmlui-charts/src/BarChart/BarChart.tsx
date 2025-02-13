64b83c92887dbb2bfc1bcd1316502c147fd4a294
captaaint <56363291+captaaint@users.noreply.github.com>
2025-02-13 20:40:07 +0100

    wip: extending chart components with new props, code refactoring, and fixes (#745)

--- packages/xmlui-charts/src/BarChart/BarChart.tsx
+++ packages/xmlui-charts/src/BarChart/BarChart.tsx
@@ -4,7 +4,6 @@ import { createComponentRenderer, createMetadata, d } from "xmlui";
 const COMP = "BarChart";
 
 export const BarChartMd = createMetadata({
-  description: `The \`BarChart\` component represents a bar chart.`,
   props: {
     data: d(
       `This property is used to provide the component with data to display. The data itself needs ` +
@@ -13,7 +12,9 @@ export const BarChartMd = createMetadata({
     dataKeys: d(
       "This property specifies the keys in the data objects that should be used for rendering the bars.",
     ),
-    stacked: d(`This property determines how the bars are layed out.`),
+    stacked: d(
+      `This property determines how the bars are laid out. If set to true, bars with the same category will be stacked on top of each other rather than placed side by side.`,
+    ),
     layout: d(
       `This property determines the orientation of the bar chart. The \`vertical\` variant ` +
         `specifies the horizontal axis as the primary and lays out the bars from left to right. ` +
@@ -21,6 +22,18 @@ export const BarChartMd = createMetadata({
         `spread from top to bottom.`,
     ),
     nameKey: d("Specifies the key in the data objects that will be used to group bars together."),
+    hideX: d(
+      "Determines whether the X-axis should be hidden. If set to true, the axis will not be rendered.",
+    ),
+    hideY: d(
+      "Determines whether the Y-axis should be hidden. If set to true, the axis will not be rendered.",
+    ),
+    hideTickX: d(
+      "Controls the visibility of the X-axis ticks. If set to true, tick labels on the X-axis will be hidden.",
+    ),
+    hideTickY: d(
+      "Controls the visibility of the Y-axis ticks. If set to true, tick labels on the Y-axis will be hidden.",
+    ),
   },
 });
 
@@ -35,7 +48,11 @@ export const barChartComponentRenderer = createComponentRenderer(
         layout={extractValue(node.props?.layout)}
         nameKey={extractValue(node.props?.nameKey)}
         dataKeys={extractValue(node.props?.dataKeys)}
-        stacked={extractValue(node.props?.stacked)}
+        stacked={extractValue.asOptionalBoolean(node.props?.stacked)}
+        hideX={extractValue.asOptionalBoolean(node.props?.hideX)}
+        hideY={extractValue.asOptionalBoolean(node.props?.hideY)}
+        hideTickX={extractValue.asOptionalBoolean(node.props?.hideTickX)}
+        hideTickY={extractValue.asOptionalBoolean(node.props?.hideTickY)}
       />
     );
   },
--- packages/xmlui-charts/src/LineChart/LineChart.tsx
+++ packages/xmlui-charts/src/LineChart/LineChart.tsx
@@ -4,11 +4,21 @@ import { LineChart } from "./LineChartNative";
 const COMP = "LineChart";
 
 export const LineChartMd = createMetadata({
-  description: "A pie chart component",
+  description: "A line chart component",
   props: {
-    data: d("The data to be displayed in the pie chart"),
-    dataKey: d("The key to use for the data"),
-    nameKey: d("The key to use for the name"),
+    data: d(
+      "The data to be displayed in the line chart. It should be an array of objects, where each object represents a data point.",
+    ),
+    dataKeys: d(
+      "This property specifies the keys in the data objects that should be used for rendering the lines.",
+    ),
+    nameKey: d("The key in the data objects used for labeling different data series."),
+    hideX: d(
+      "Determines whether the X-axis should be hidden. If set to true, the axis will not be displayed.",
+    ),
+    hideTooltip: d(
+      "Determines whether the tooltip should be hidden. If set to true, no tooltip will be shown when hovering over data points.",
+    ),
   },
 });
 
@@ -20,8 +30,10 @@ export const lineChartComponentRenderer = createComponentRenderer(
       <LineChart
         data={extractValue(node.props?.data)}
         style={layoutCss}
-        dataKey={extractValue(node.props?.dataKey)}
+        dataKeys={extractValue(node.props?.dataKeys)}
         nameKey={extractValue(node.props?.nameKey)}
+        hideX={extractValue(node.props?.hideX)}
+        hideTooltip={extractValue(node.props?.hideTooltip)}
       />
     );
   },
