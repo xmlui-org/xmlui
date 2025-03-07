869157daf14b827487521a02c82e6df11996b54b
Marcell Kiss <52428841+marcello-tk@users.noreply.github.com>
2025-03-07 13:53:32 +0000

    docs: readd pdf ext package (#879)

--- packages/xmlui-pdf/meta/componentsMetadata.ts
+++ packages/xmlui-pdf/meta/componentsMetadata.ts
@@ -3,6 +3,7 @@ import { PdfMd } from "../src";
 export const componentMetadata = {
   name: "PDF",
   state: "in progress",
+  description: "This package provides components for working with pdf files.",
   metadata: {
     Pdf: PdfMd,
   },
