385584f58285fccc879ca9af2ab7e8ad24ca1f1e
captaaint <56363291+captaaint@users.noreply.github.com>
2025-03-27 13:27:34 +0100

    wip

--- xmlui/src/components-core/DevTools.tsx
+++ xmlui/src/components-core/DevTools.tsx
@@ -12,6 +12,7 @@ import { BiDockBottom, BiDockLeft, BiDockRight } from "react-icons/bi";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { HiOutlineDotsVertical } from "react-icons/all";
 import { HiOutlineClipboardDocument, HiOutlineClipboardDocumentCheck } from "react-icons/hi2";
+import Icon from "../components/Icon/IconNative";
 
 type Props = {
   setIsOpen: (isOpen: boolean) => void;
@@ -200,15 +201,19 @@ export const DevTools = ({ setIsOpen, node }: Props) => {
                 </DropdownMenu.Content>
               </DropdownMenu.Portal>
             </DropdownMenu.Root>
-            <Button onClick={() => setIsOpen(false)} size={"xs"} variant={"ghost"}>
-              Close
-            </Button>
+            <Button onClick={() => setIsOpen(false)} size={"xs"} variant={"ghost"} icon={<Icon name={"close"} />}/>
           </div>
         </List>
         <Content value={"code"} className={styles.content}>
           <XmluiCodeHighlighter value={value} />
           <div className={styles.copyButton}>
-            <Button onClick={copyToClipboard} variant={"solid"} themeColor={"secondary"} style={{ padding: 8 }} size={"sm"}>
+            <Button
+              onClick={copyToClipboard}
+              variant={"solid"}
+              themeColor={"secondary"}
+              style={{ padding: 8 }}
+              size={"sm"}
+            >
               {copied ? (
                 <HiOutlineClipboardDocumentCheck size={16} />
               ) : (
