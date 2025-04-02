0437eac4d7112ff1b2f38b78ce2a28ba2ca65749
Istvan Novak <dotneteer@hotmail.com>
2025-04-02 14:46:01 +0200

    wip

--- xmlui/src/abstractions/scripting/Compilation.ts
+++ xmlui/src/abstractions/scripting/Compilation.ts
@@ -1,17 +1,52 @@
 import { ComponentDef, CompoundComponentDef } from "../ComponentDefs";
 import { ThemeDefinition } from "../ThemingDefs";
+import { Expression } from "./ScriptingSourceTree";
 
+// --- Contains the compilation result of a project
 export type ProjectCompilation = {
+  // --- The compiled Main.xmlui file (with its optional code behind)
   entrypoint: FileCompilation;
-  components: FileCompilation[];
-  themes: Record<string, ThemeDefinition>;
-}
 
+  // --- The compiled component files (with their optional code behind)
+  components: FileCompilation[];
+
+  // --- The compiled theme files
+  themes: Record<string, ThemeDefinition>;
+};
+
+// --- The compilation result of a single file
 export type FileCompilation = {
+  // --- The file name
   filename: string;
+
+  // --- The component name (if the component is a compound component)
   componentName?: string;
+
+  // --- The compiled markup of the main file or component file
   definition: ComponentDef | CompoundComponentDef;
+
+  // --- Optional markup source (used in dev mode)
   markupSource?: string;
+
+  // --- Optional code behind source (used in dev mode)
   codeBehindSource?: string;
+
+  // --- Other (non-core) component names this component depends on
   dependencies: string[];
-}
+};
+
+// --- The parsed property value (if defined by an attribute value)
+export type ParsedPropertyValue = {
+  // --- We recognize this as a parsed property value
+  __PARSED: true;
+
+  // --- The property segments
+  segments?: PropertySegment[];
+};
+
+// --- The compliation result of a single property
+export type PropertySegment = {
+  literal?: string;
+  expr?: Expression;
+  deps?: string[];
+};
---
+++ xmlui/src/components-core/script-runner/AttributeValueParser.ts
@@ -0,0 +1,116 @@
+import { ParsedPropertyValue } from "../../abstractions/scripting/Compilation";
+import { Expression } from "../../abstractions/scripting/ScriptingSourceTree";
+import { Parser } from "../../parsers/scripting/Parser";
+
+/**
+ * This function parses a parameter string and splits them into string literal and binding expression sections
+ * @param source String to parse
+ * @returns Parameter string sections
+ */
+export function parseAttributeValue(source: string): ParsedPropertyValue {
+  const result: ParsedPropertyValue = {
+    __PARSED: true,
+    segments: [],
+  };
+  if (source === undefined || source === null) return result;
+
+  let phase = ParsePhase.StringLiteral;
+  let section = "";
+  let escape = "";
+  for (let i = 0; i < source.length; i++) {
+    const ch = source[i];
+    switch (phase) {
+      case ParsePhase.StringLiteral:
+        if (ch === "\\") {
+          phase = ParsePhase.Escape;
+          escape = "\\";
+        } else if (ch === "{") {
+          // --- A new expression starts, close the previous string literal
+          if (section !== "") {
+            result.segments.push({
+              literal: section,
+            });
+          }
+          // --- Start a new section
+          section = "";
+          phase = ParsePhase.ExprStart;
+        } else {
+          section += ch;
+        }
+        break;
+
+      case ParsePhase.Escape:
+        if (ch === "\\") {
+          // --- Go on with escape
+          escape += ch;
+          break;
+        }
+
+        if (ch === "{") {
+          // --- End escape as a literal section without the first "\" escape character
+          section += escape.substring(1) + ch;
+        } else {
+          // --- End escape as a literal section with the full sequence
+          section += escape + ch;
+        }
+        phase = ParsePhase.StringLiteral;
+        break;
+
+      case ParsePhase.ExprStart:
+        const exprSource = source.substring(i);
+        const parser = new Parser(source.substring(i));
+        let expr: Expression | null = null;
+        try {
+          expr = parser.parseExpr();
+        } catch (err) {
+          throw new Error(`Cannot parse expression: '${exprSource}': ${err}`);
+        }
+        const tail = parser.getTail();
+        if (!tail || tail.trim().length < 1 || tail.trim()[0] !== "}") {
+          // --- Unclosed expression, back to its beginning
+          throw new Error(`Unclosed expression: '${source}'\n'${exprSource}'`);
+        } else {
+          // --- Successfully parsed expression, get dependencies
+          result.segments.push({
+            expr,
+          });
+
+          // --- Skip the parsed part of the expression, and start a new literal section
+          i = source.length - tail.length;
+          section = "";
+        }
+        phase = ParsePhase.StringLiteral;
+        break;
+    }
+  }
+
+  // --- Process  the last segment
+  switch (phase) {
+    case ParsePhase.StringLiteral:
+      if (section !== "") {
+        result.segments.push({
+          literal: section,
+        });
+      }
+      break;
+    case ParsePhase.Escape:
+      result.segments.push({
+        literal: section + escape,
+      });
+      break;
+    case ParsePhase.ExprStart:
+      result.segments.push({
+        literal: section + "{",
+      });
+      break;
+  }
+
+  // --- Done.
+  return result;
+}
+
+enum ParsePhase {
+  StringLiteral,
+  Escape,
+  ExprStart,
+}
