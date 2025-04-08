dea2357b785d46109ca3e78e3192dde30ff4925a
Norbert Hajagos <hajagosnorbi@gmail.com>
2025-04-08 18:25:15 +0200

    remove packages and unnecesarry code

--- packages/editor-demo/src/vite-env.d.ts
+++ packages/editor-demo/src/vite-env.d.ts
@@ -1,8 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) 2024 TypeFox and others.
- * Licensed under the MIT License. See LICENSE in the package root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 /// <reference types="vite/client" />
 
 declare module '*?raw' {
