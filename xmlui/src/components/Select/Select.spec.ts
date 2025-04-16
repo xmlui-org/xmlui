97c40506fa7a1051403adcbae1d53b5298abd99f
Istvan Novak <dotneteer@hotmail.com>
2025-04-16 07:36:16 +0200

    fix: Select inityialvalue="{0}" works

--- xmlui/src/components/Select/Select.spec.ts
+++ xmlui/src/components/Select/Select.spec.ts
@@ -1,6 +1,9 @@
 import { expect, test } from "../../testing/fixtures";
 
-test("options with number type keeps number type - outside of forms", async ({ initTestBed, createSelectDriver }) => {
+test("options with number type keeps number type - outside of forms", async ({
+  initTestBed,
+  createSelectDriver,
+}) => {
   const { testStateDriver } = await initTestBed(
     `<Select onDidChange="(value) => {console.log('bla'); testState = value; }">
       <Option value="{1}" label="One"/>
@@ -13,15 +16,65 @@ test("options with number type keeps number type - outside of forms", async ({ i
   await expect.poll(testStateDriver.testState).toStrictEqual(1);
 });
 
-test("options with number type keeps number type - inside a form", async ({ initTestBed, createSelectDriver }) => {
+test("options with number type keeps number type - inside a form", async ({
+  initTestBed,
+  createSelectDriver,
+}) => {
   const { testStateDriver } = await initTestBed(`
     <Select onDidChange="(value) => testState = value">
       <Option value="{1}" label="One"/>
       <Option value="{2}" label="Two"/>
-    </Select>`
-  );
+    </Select>`);
   const driver = await createSelectDriver();
 
   await driver.selectOption("One");
   await expect.poll(testStateDriver.testState).toStrictEqual(1);
+});
+
+test("select initialValue='{0}' works", async ({ page, initTestBed, createSelectDriver }) => {
+  const { testStateDriver } = await initTestBed(`
+    <Fragment>
+      <Select id="mySelect" initialValue="{0}">
+        <Option value="{0}" label="Zero"/>
+        <Option value="{1}" label="One"/>
+        <Option value="{2}" label="Two"/>
+      </Select>
+      <Text testId="text">Selected value: {mySelect.value}</Text>
+    </Fragment>
+  `);
+  const driver = await createSelectDriver();
+
+  await expect(page.getByTestId("text")).toHaveText("Selected value: 0");
+});
+
+test("multi-select initialValue='{[0]}' works", async ({ page, initTestBed, createSelectDriver }) => {
+  await initTestBed(`
+    <Fragment>
+      <Select id="mySelect" initialValue="{[0]}" multiSelect>
+        <Option value="{0}" label="Zero"/>
+        <Option value="{1}" label="One"/>
+        <Option value="{2}" label="Two"/>
+      </Select>
+      <Text testId="text">Selected value: {mySelect.value}</Text>
+    </Fragment>
+  `);
+  const driver = await createSelectDriver();
+
+  await expect(page.getByTestId("text")).toHaveText("Selected value: 0");
+});
+
+test("multi-select initialValue='{[0,1]}' works", async ({ page, initTestBed, createSelectDriver }) => {
+  await initTestBed(`
+    <Fragment>
+      <Select id="mySelect" initialValue="{[0,1]}" multiSelect>
+        <Option value="{0}" label="Zero"/>
+        <Option value="{1}" label="One"/>
+        <Option value="{2}" label="Two"/>
+      </Select>
+      <Text testId="text">Selected value: {mySelect.value}</Text>
+    </Fragment>
+  `);
+  const driver = await createSelectDriver();
+
+  await expect(page.getByTestId("text")).toHaveText("Selected value: 0,1");
 });
