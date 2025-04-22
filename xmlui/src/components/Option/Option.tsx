cacc44068ad515153cf1d3e5b60580a6739103eb
captaaint <56363291+captaaint@users.noreply.github.com>
2025-04-22 13:12:03 +0200

    fix: select option (#1083)

--- xmlui/src/components/Option/Option.tsx
+++ xmlui/src/components/Option/Option.tsx
@@ -11,22 +11,21 @@ const COMP = "Option";
 
 export const OptionMd = createMetadata({
   description:
-      `\`${COMP}\` is a non-visual component describing a selection option. Other components ` +
-      `(such as \`Select\`, \`AutoComplete\`, and others) may use nested \`Option\` instances ` +
-      `from which the user can select.`,
+    `\`${COMP}\` is a non-visual component describing a selection option. Other components ` +
+    `(such as \`Select\`, \`AutoComplete\`, and others) may use nested \`Option\` instances ` +
+    `from which the user can select.`,
   props: {
     label: d(
-        `This property defines the text to display for the option. If \`label\` is not defined, ` +
+      `This property defines the text to display for the option. If \`label\` is not defined, ` +
         `\`Option\` will use the \`value\` as the label.`,
     ),
     value: d(
-        `This property defines the value of the option. If \`value\` is not defined, ` +
+      `This property defines the value of the option. If \`value\` is not defined, ` +
         `\`Option\` will use the \`label\` as the value.`,
     ),
     enabled: dEnabled(),
     optionTemplate: d("This property is used to define a custom option template"),
   },
-  childrenAsTemplate: "optionTemplate"
 });
 
 const OptionNative = memo((props: Option) => {
@@ -39,29 +38,30 @@ const OptionNative = memo((props: Option) => {
 OptionNative.displayName = "OptionNative";
 
 export const optionComponentRenderer = createComponentRenderer(
-    COMP,
-    OptionMd,
-    ({ node, extractValue, layoutCss, renderChild, layoutContext }) => {
-      const optionTemplate = node.props.optionTemplate;
-      return (
-          <OptionNative
-              optionRenderer={
-                optionTemplate
-                    ? (contextVars) => (
-                        <MemoizedItem
-                            node={optionTemplate}
-                            renderChild={renderChild}
-                            contextVars={contextVars}
-                            layoutContext={layoutContext}
-                        />
-                    )
-                    : undefined
-              }
-              value={extractValue(node.props.value)}
-              label={extractValue.asOptionalString(node.props.label) || renderChild(node.children)}
-              enabled={extractValue.asOptionalBoolean(node.props.enabled)}
-              style={layoutCss}
-          />
-      );
-    },
+  COMP,
+  OptionMd,
+  ({ node, extractValue, layoutCss, renderChild, layoutContext }) => {
+    const optionTemplate = node.props.optionTemplate;
+
+    return (
+      <OptionNative
+        optionRenderer={
+          optionTemplate
+            ? (contextVars) => (
+                <MemoizedItem
+                  node={optionTemplate}
+                  renderChild={renderChild}
+                  contextVars={contextVars}
+                  layoutContext={layoutContext}
+                />
+              )
+            : undefined
+        }
+        value={extractValue(node.props.value)}
+        label={extractValue.asOptionalString(node.props.label) || renderChild(node.children)}
+        enabled={extractValue.asOptionalBoolean(node.props.enabled)}
+        style={layoutCss}
+      />
+    );
+  },
 );
