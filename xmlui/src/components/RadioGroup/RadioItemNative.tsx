82b709c0095e6a2fd0fec02bea3de8ac1f64ae37
captaaint <56363291+captaaint@users.noreply.github.com>
2025-04-29 18:22:20 +0200

    wip

--- xmlui/src/components/RadioGroup/RadioItemNative.tsx
+++ xmlui/src/components/RadioGroup/RadioItemNative.tsx
@@ -5,19 +5,24 @@ import * as InnerRadioGroup from "@radix-ui/react-radio-group";
 import classnames from "classnames";
 
 type Props = {
-    checked: boolean;
-    style?: React.CSSProperties;
-    value?: string;
+  checked: boolean;
+  style?: React.CSSProperties;
+  value?: string;
 };
 
 export const RadioItem = ({ checked, style, value }: Props) => {
-    const id = useId();
+  const id = useId();
 
-    return (
-        <div key={id} className={styles.radioOptionContainer} style={style}>
-            <InnerRadioGroup.Item className={classnames(styles.radioOption)} id={id} value={value} checked={checked}>
-                <InnerRadioGroup.Indicator className={classnames(styles.indicator)} />
-            </InnerRadioGroup.Item>
-        </div>
-    );
+  return (
+    <div key={id} className={styles.radioOptionContainer} style={style}>
+      <InnerRadioGroup.Item
+        className={classnames(styles.radioOption)}
+        id={id}
+        value={value}
+        checked={checked}
+      >
+        <InnerRadioGroup.Indicator className={classnames(styles.indicator)} />
+      </InnerRadioGroup.Item>
+    </div>
+  );
 };
