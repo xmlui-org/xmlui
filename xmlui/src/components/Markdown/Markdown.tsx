6976218006e08f4b3782f607eca6c6460c14c35c
gergelyilles <illes.gergo@gmail.com>
2025-05-06 09:34:20 +0200

    resolve merge

--- xmlui/src/components/Markdown/Markdown.tsx
+++ xmlui/src/components/Markdown/Markdown.tsx
@@ -4,10 +4,11 @@ import { createMetadata, d } from "../../abstractions/ComponentDefs";
 import { createComponentRenderer } from "../../components-core/renderers";
 import { parseScssVar } from "../../components-core/theming/themeVars";
 import { Markdown } from "./MarkdownNative";
+import type React from "react";
 import { useMemo } from "react";
 import type { ValueExtractor } from "../../abstractions/RendererDefs";
 import { parseBindingExpression } from "./parse-binding-expr";
-import { CodeHighlighter } from "./highlight-code";
+import type { CodeHighlighter } from "./highlight-code";
 import { convertPlaygroundPatternToMarkdown, observePlaygroundPattern } from "./utils";
 
 const COMP = "Markdown";
@@ -111,28 +112,31 @@ type TransformedMarkdownProps = {
   codeHighlighter?: CodeHighlighter;
 };
 
-const TransformedMarkdown = ({ children, removeIndents, style, extractValue }: TransformedMarkdownProps) => {
-  const markdownContent = useMemo(()=>{
+const TransformedMarkdown = ({
+  children,
+  removeIndents,
+  style,
+  extractValue,
+}: TransformedMarkdownProps) => {
+  const markdownContent = useMemo(() => {
     if (typeof children !== "string") {
       return null;
     }
 
-    // --- Resolve bindig expression values
-    const withBindinxExprs = parseBindingExpression(children, extractValue);
-
+    // --- Resolve binding expression values
     // --- Resolve xmlui playground definitions
-    let resolvedMd = withBindinxExprs;
+    let resolvedMd = parseBindingExpression(children, extractValue);
     while (true) {
       const nextPlayground = observePlaygroundPattern(resolvedMd);
       if (!nextPlayground) break;
 
-      resolvedMd = resolvedMd.slice(0, nextPlayground[0]) +
+      resolvedMd =
+        resolvedMd.slice(0, nextPlayground[0]) +
         convertPlaygroundPatternToMarkdown(nextPlayground[2]) +
         resolvedMd.slice(nextPlayground[1]);
     }
     // console.log(resolvedMd)
     return resolvedMd;
-
   }, [children, extractValue]);
 
   return (
