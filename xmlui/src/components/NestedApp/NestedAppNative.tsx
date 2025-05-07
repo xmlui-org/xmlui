ccea757f14c5bdfac772ee22d4cc6ddd7e60ed67
Istvan Novak <dotneteer@hotmail.com>
2025-05-07 06:16:58 +0200

    wip: component reference docs-related changes (#1147)

--- xmlui/src/components/NestedApp/NestedAppNative.tsx
+++ xmlui/src/components/NestedApp/NestedAppNative.tsx
@@ -47,7 +47,6 @@ export function NestedApp({
   const toneToApply = activeTone || config?.defaultTone || theme?.activeThemeTone;
 
   const apiWorker = useMemo(() => {
-    // console.log("HERE");
     if (typeof document !== "undefined") {
       return setupWorker();
     }
@@ -86,8 +85,7 @@ export function NestedApp({
       component = errReportComponent(errors, "Main.xmlui", erroneousCompoundComponentName);
     }
     const compoundComponents: CompoundComponentDef[] =
-      components ??
-      [].map((src) => {
+      (components ?? []).map((src) => {
         const isErrorReportComponent = typeof src !== "string";
         if (isErrorReportComponent) {
           return src;
