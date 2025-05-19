9a74335526e9cf78d5049ad325e9cb83cadfd9b5
Gergely Illés <illes.gergo@gmail.com>
2025-05-19 12:33:33 +0200

    docs cd (#1225)

--- xmlui/bin/index.ts
+++ xmlui/bin/index.ts
@@ -22,8 +22,8 @@ async function zipDirectory(sourceDir: string, outPath: string = sourceDir) {
   console.log(`Zip file created: ${outPath}`);
 }
 
-async function zipDist() {
-  await zipDirectory(process.cwd() + "/dist", `${process.cwd()}/ui.zip`);
+async function zipDist({target = "ui.zip"}: {target?: string}) {
+  await zipDirectory(process.cwd() + "/dist", `${process.cwd()}/${target}`);
 }
 
 function dedupeArg(arg: any) {
@@ -77,7 +77,8 @@ switch (script) {
     break;
   }
   case "zip-dist": {
-    zipDist();
+    const { target } = argv as any;
+    zipDist({target});
     break;
   }
   default: {
