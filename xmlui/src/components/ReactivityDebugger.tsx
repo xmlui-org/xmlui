45ec9040a48daa1cb43b48a70d3f5b7298f45801
Jon Udell <jon@jonudell.info>
2025-05-26 09:38:51 -0700

    add placeholder ReactivityDebugger

---
+++ xmlui/src/components/ReactivityDebugger.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ReactivityDebugger = () => {
+  return <div>Reactivity Debugger Placeholder</div>;
+};
+
+export const reactivityDebuggerComponentRenderer = {
+  type: "ReactivityDebugger",
+  renderer: ReactivityDebugger,
+};
