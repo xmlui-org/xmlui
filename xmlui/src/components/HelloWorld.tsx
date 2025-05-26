d2f106224999f5c84bb296b4320dd5bfd75997bf
Jon Udell <jon@jonudell.info>
2025-05-25 20:25:55 -0700

    log var resolution in StateContainer.tsx

--- xmlui/src/components/HelloWorld.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createComponentRenderer } from "../components-core/renderers";
-
-export const reactivityDebuggerComponentRenderer = createComponentRenderer(
-  "ReactivityDebugger",
-  undefined,
-  () => <div>Reactivity Debugger</div>
-);
