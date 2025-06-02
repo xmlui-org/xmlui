2d09699572fabb835e470be63dc34241095afe47
captaaint <56363291+captaaint@users.noreply.github.com>
2025-06-02 18:19:07 +0200

    wip

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -201,8 +201,8 @@ const App: StandaloneAppDescription = {
   name: "XMLUI docs",
   defaultTheme: "default",
   resources: {
-    logo: "/resources/xmlui-logo.svg",
-    "logo-dark": "/resources/xmlui-logo-dark.svg",
+    logo: "/resources/logo.svg",
+    "logo-dark": "/resources/logo-dark.svg",
     favicon: "/resources/favicon.ico",
     "font.Inter":
       "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
