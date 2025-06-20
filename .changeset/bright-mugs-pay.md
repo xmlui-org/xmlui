d507ea831811019bee99e63e9c80906bb6ef9d5c
Istvan Novak <dotneteer@hotmail.com>
2025-06-20 14:24:53 +0200

    feat: Add AppWithCodeView component to display code and running app side-by-side (#1445)

---
+++ .changeset/bright-mugs-pay.md
@@ -0,0 +1,5 @@
+---
+"xmlui": patch
+---
+
+Add AppWithCodeView component to display code and running app side-by-side
---
+++ xmlui/src/components/NestedApp/AppWithCodeViewNative.tsx
@@ -0,0 +1,79 @@
+import { type ReactNode } from "react";
+import styles from "./AppWithCodeView.module.scss";
+import classnames from "classnames";
+import { NestedApp } from "./NestedAppNative";
+import { Markdown } from "../Markdown/Markdown";
+import type { ThemeTone } from "../../abstractions/ThemingDefs";
+import { Stack } from "../Stack/StackNative";
+
+type AppWithCodeViewNativeProps = {
+  /**
+   * Markdown content to display in the left column
+   */
+  markdown: string;
+  /**
+   * Display layout in side-by-side mode (horizontal) when true,
+   * or stacked (vertical) when false or undefined
+   */
+  sideBySide?: boolean;
+  api?: any;
+  app: string;
+  components?: any[];
+  config?: any;
+  activeTone?: ThemeTone;
+  activeTheme?: string;
+  title?: string;
+  height?: string | number;
+  allowPlaygroundPopup?: boolean;
+  withFrame?: boolean;
+};
+
+/**
+ * A component that displays markdown content on the left and a NestedApp on the right
+ */
+export function AppWithCodeViewNative({
+  markdown,
+  sideBySide,
+  app,
+  api,
+  components = [],
+  config,
+  activeTone,
+  activeTheme,
+  title,
+  height,
+  allowPlaygroundPopup,
+  withFrame,
+}: AppWithCodeViewNativeProps): ReactNode {
+  return (
+    <div
+      className={classnames(styles.container, {
+        [styles.horizontal]: sideBySide,
+        [styles.vertical]: !sideBySide,
+      })}
+    >
+      <div className={classnames({ [styles.column]: sideBySide })}>
+        <Markdown>{markdown}</Markdown>
+      </div>
+      <div className={classnames({ [styles.column]: sideBySide })}>
+        <NestedApp
+          app={app}
+          api={api}
+          components={components}
+          config={config}
+          activeTone={activeTone}
+          activeTheme={activeTheme}
+          title={title}
+          height={height}
+          allowPlaygroundPopup={allowPlaygroundPopup}
+          withFrame={sideBySide ? false : withFrame}
+        />
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Export a named default for easier imports
+ */
+export default AppWithCodeViewNative;
