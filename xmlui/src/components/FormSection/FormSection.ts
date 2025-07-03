4f9ff06a12eda987ffbb3fee8bdb16cffb910a1f
Istvan Novak <dotneteer@hotmail.com>
2025-07-03 06:18:56 +0200

    fix the build issue with FormSection (#1539)

--- xmlui/src/components/FormSection/FormSection.ts
+++ xmlui/src/components/FormSection/FormSection.ts
@@ -1,7 +1,8 @@
 import { CompoundComponentRendererInfo } from "../../abstractions/RendererDefs";
 import { compoundComponentDefFromSource } from "../../components-core/utils/compound-utils";
 import { createMetadata } from "../metadata-helpers";
-import componentSource from "./FormSection.xmlui?raw";
+// --- We cannot use this with nextra
+// import componentSource from "./FormSection.xmlui?raw";
 
 const COMP = "FormSection";
 
@@ -11,6 +12,31 @@ export const FormSectionMd = createMetadata({
     "`FormSection` groups elements within a `Form`. Child components are placed in " +
     "a [FlowLayout](/components/FlowLayout).",
 });
+
+const componentSource = `
+<Component name="FormSection">
+  <VStack paddingBottom="{$props.paddingBottom ?? '1rem'}" gap="0" width="100%">
+    <Heading 
+      when="{!!$props.heading}"
+      marginBottom="$space-tight"
+      level="{$props.headingLevel ?? 'h3'}"
+      fontWeight="{$props.headingWeight ?? 'bold'}"
+      value="{$props.heading}" />
+    <Text
+      when="{!!$props.info}"
+      fontSize="{$props.infoFontSize ?? '0.8rem'}"
+      paddingBottom="$space-normal"
+      value="{$props.info}" />
+    <FlowLayout 
+      width="100%"
+      paddingTop="{$props.paddingTop ?? '$space-normal'}"
+      columnGap="{$props.columnGap ?? '3rem'}"
+      rowGap="{$props.rowGap ?? '$space-normal'}" >
+      <Slot />
+    </FlowLayout>
+  </VStack>
+</Component>
+`;
 
 export const formSectionRenderer: CompoundComponentRendererInfo = {
   compoundComponentDef: compoundComponentDefFromSource(COMP, componentSource),
