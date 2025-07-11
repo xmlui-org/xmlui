c20f9e8db3339fb94dfa0e916a0cd0ba70cf069f
marcello-tk <awesomedevguynull@gmail.com>
2025-07-11 09:58:19 +0100

    test: add a couple of e2e cases for CodeBlock

--- xmlui/src/components/CodeBlock/CodeBlock.spec.ts
+++ xmlui/src/components/CodeBlock/CodeBlock.spec.ts
@@ -1,11 +1,8 @@
-import { SKIP_REASON } from "../../testing/component-test-helpers";
 import { expect, test } from "../../testing/fixtures";
-import type { ComponentDriver } from "../../testing/ComponentDrivers";
-
 
 // --- Smoke
 
-/* test.describe("smoke tests", { tag: "@smoke" }, () => {
+test.describe("smoke tests", { tag: "@smoke" }, () => {
   test("component renders", async ({ initTestBed, createCodeBlockDriver }) => {
     await initTestBed(`<CodeBlock />`);
     await expect((await createCodeBlockDriver()).component).toBeAttached();
@@ -14,27 +11,11 @@ import type { ComponentDriver } from "../../testing/ComponentDrivers";
   test("component with Text codefence renders", async ({ initTestBed, createCodeBlockDriver }) => {
     await initTestBed(`
     <CodeBlock>
-      <Text uid={id} variant="codefence">
+      <Text variant="codefence">
         This is a test
       </Text>
     </CodeBlock>
     `);
     await expect((await createCodeBlockDriver()).component).toBeAttached();
   });
-}); */
-
-test("component renders", async ({ initTestBed, createCodeBlockDriver }) => {
-  await initTestBed(`<CodeBlock />`);
-  await expect((await createCodeBlockDriver()).component).toBeAttached();
-});
-
-test("component with Text codefence renders", async ({ initTestBed, createCodeBlockDriver }) => {
-  await initTestBed(`
-  <CodeBlock>
-    <Text variant="codefence">
-      This is a test
-    </Text>
-  </CodeBlock>
-  `);
-  await expect((await createCodeBlockDriver()).component).toBeAttached();
 });
