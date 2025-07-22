3e19b6cc78a9653916b86f5f3826e6a41ed86239
Norbert Hajagos <hajagosnorbi@gmail.com>
2025-07-22 12:24:10 +0200

    fix: remove type annotation that depends on a package not present

--- xmlui/src/syntax/monaco/xmlui-light.ts
+++ xmlui/src/syntax/monaco/xmlui-light.ts
@@ -1,6 +1,8 @@
-import type { editor } from "monaco-editor"
+// import type { editor } from "monaco-editor"
 
-const lightTheme: editor.IStandaloneThemeData = {
+// todo: after unification of the grammar files, put this back in.
+// const lightTheme: editor.IStandaloneThemeData = {
+const lightTheme: any = {
   base: "vs",
   inherit: true,
   rules: [
@@ -12,8 +14,8 @@ const lightTheme: editor.IStandaloneThemeData = {
     { token: "attribute", foreground: "#8C150D" },
     { token: "operators", foreground: "#7235C7" },
     { token: "delimiter.angle", foreground: "#66748E" },
-    { token: "delimiter.curly",  foreground: "#FF9536", fontStyle: "bold" },
-    { token: "string", foreground:  "#497A48"},
+    { token: "delimiter.curly", foreground: "#FF9536", fontStyle: "bold" },
+    { token: "string", foreground: "#497A48" },
     { token: "tag-cdata", foreground: "#1BADFF" },
     { token: "tag-script", foreground: "#0CA6A6" },
     { token: "string.escape", foreground: "#3CBC89" },
