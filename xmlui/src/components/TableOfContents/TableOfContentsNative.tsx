1e9d041c2fe7535664072e147b0eeba2017993a0
Gergely Illés <illes.gergo@gmail.com>
2025-08-19 11:04:19 +0200

    ToC style fixes (#1905)

--- xmlui/src/components/TableOfContents/TableOfContentsNative.tsx
+++ xmlui/src/components/TableOfContents/TableOfContentsNative.tsx
@@ -18,6 +18,7 @@ import { useIsomorphicLayoutEffect } from "../../components-core/utils/hooks";
 
 type Props = {
   style?: CSSProperties;
+  className?: string;
   smoothScrolling?: boolean;
   maxHeadingLevel?: number;
   omitH1?: boolean;
@@ -35,6 +36,7 @@ export const TableOfContents = forwardRef(function TableOfContents(
     smoothScrolling = defaultProps.smoothScrolling,
     maxHeadingLevel = defaultProps.maxHeadingLevel,
     omitH1 = defaultProps.omitH1,
+    className
   }: Props,
   forwardedRef: ForwardedRef<HTMLDivElement>,
 ) {
@@ -74,7 +76,7 @@ export const TableOfContents = forwardRef(function TableOfContents(
   }, [activeAnchorId, headings]);
 
   return (
-    <div className={styles.nav} ref={ref} style={style}>
+    <div className={classnames(styles.nav, className)} ref={ref} style={style}>
       <ul className={styles.list}>
         {headings.map((value) => {
           if (value.level <= maxHeadingLevel && (!omitH1 || value.level !== 1)) {
