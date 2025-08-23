0f8c9b5f493dbc9a4cffd81e7927d7ba74b4a775
Jon Udell <jon@jonudell.info>
2025-08-22 22:54:56 -0700

    wip

--- docs/extensions.ts
+++ docs/extensions.ts
@@ -1,4 +1,5 @@
 import playground from "xmlui-playground";
 import search from "xmlui-search";
+import helloWorld from "xmlui-hello-world";
 
-export default [playground, search];
+export default [playground, search, helloWorld];
