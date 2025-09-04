149dbd7fac18524f0d7964edab4a40f173c58a93
Istvan Novak <dotneteer@hotmail.com>
2025-09-04 14:42:45 +0200

    Add the appendBlob function to the ApiInterceptor backend

--- xmlui/src/components-core/interception/Backend.ts
+++ xmlui/src/components-core/interception/Backend.ts
@@ -126,6 +126,13 @@ export class Backend {
         delay,
         Errors,
         createFile: (...args: any[]) => new File(args[0], args[1], args[2]),
+        appendBlob: (blob1: Blob | undefined | null, blob2: Blob) => {
+          if (!blob1 && !blob2) return;
+          const newBlob = new Blob([blob1, blob2], {
+            type: blob1?.type || blob2?.type || "application/octet-stream",
+          });
+          return newBlob;
+        },
         getDate,
       },
     });
