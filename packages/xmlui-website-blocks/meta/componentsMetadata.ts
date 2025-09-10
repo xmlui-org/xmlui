8a3d5cd26d6088a397d4a63d32a685c15ad5f1b0
Istvan Novak <dotneteer@hotmail.com>
2025-09-10 18:15:34 +0200

    Add Google Analytics to the docs  (#2017)

--- packages/xmlui-website-blocks/meta/componentsMetadata.ts
+++ packages/xmlui-website-blocks/meta/componentsMetadata.ts
@@ -1,12 +1,12 @@
-import { HelloMd } from "../src/Hello/Hello"; 
 import { HeroSectionMd } from "../src/HeroSection/HeroSection"
+import { ScrollToTopMd } from "../src/ScrollToTop/ScrollToTop"
 
 export const componentMetadata = {
   name: "Websites",
   state: "experimental",
   description: "This package contains components...",
   metadata: {
-    Hello: HelloMd,
     HeroSection: HeroSectionMd,
+    ScrollToTop: ScrollToTopMd,
   },
 };
--- packages/xmlui-website-blocks/src/index.tsx
+++ packages/xmlui-website-blocks/src/index.tsx
@@ -1,7 +1,7 @@
-import { helloComponentRenderer } from "./Hello/Hello";
 import { heroSectionComponentRenderer } from "./HeroSection/HeroSection";
+import { scrollToTopComponentRenderer } from "./ScrollToTop/ScrollToTop";
 
 export default {
   namespace: "XMLUIExtensions",
-  components: [helloComponentRenderer, heroSectionComponentRenderer],
+  components: [heroSectionComponentRenderer, scrollToTopComponentRenderer],
 };
