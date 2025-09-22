243b7fa2b7655cc85ae60d96b679a92ee62aaa71
captaaint <56363291+captaaint@users.noreply.github.com>
2025-09-22 10:27:49 +0200

    fix: modal dialog/toast issue (#2086)

---
+++ .changeset/tender-impalas-tell.md
@@ -0,0 +1,5 @@
+---
+"xmlui": patch
+---
+
+fix: modal dialog/toast issue
--- xmlui/src/components/Theme/NotificationToast.tsx
+++ xmlui/src/components/Theme/NotificationToast.tsx
@@ -1,5 +1,7 @@
 import { CSSProperties, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 import toast, { ToastBar, Toaster } from "react-hot-toast";
+import { useTheme } from "../../components-core/theming/ThemeContext";
 
 const TOASTER_CONTAINER_STYLE: CSSProperties = {
   top: 40,
@@ -18,6 +20,7 @@ let toasterMounted = false;
 
 export const NotificationToast = ({ toastDuration }: NotificationToastProps) => {
   const [shouldRender, setShouldRender] = useState(false);
+  const { root } = useTheme();
 
   useEffect(() => {
     if (!toasterMounted) {
@@ -27,7 +30,7 @@ export const NotificationToast = ({ toastDuration }: NotificationToastProps) => 
   }, []);
 
   if (!shouldRender) return null;
-  return (
+  return createPortal(
     <Toaster
       position={"top-right"}
       containerStyle={TOASTER_CONTAINER_STYLE}
@@ -45,6 +48,7 @@ export const NotificationToast = ({ toastDuration }: NotificationToastProps) => 
           </ToastBar>
         </div>
       )}
-    </Toaster>
+    </Toaster>,
+    root,
   );
 };
