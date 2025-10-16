91b169701137750287c7554471ba3f52b266125b
Norbert Hajagos <hajagosnorbi@gmail.com>
2025-10-16 11:05:24 +0200

    handles trivia between sibling taglike nodes

--- xmlui/src/language-server/services/common/syntax-node-utilities.ts
+++ xmlui/src/language-server/services/common/syntax-node-utilities.ts
@@ -1,52 +1,58 @@
-import type { Node, GetText} from "../../../parsers/xmlui-parser";
+import type { Node, GetText } from "../../../parsers/xmlui-parser";
 import { SyntaxKind } from "../../../parsers/xmlui-parser";
 
-export function findTagNameNodeInStack(nodeStack: Node[]): Node{
-
-  const elementNode = nodeStack.findLast(n => n.kind === SyntaxKind.ElementNode);
-  if (!elementNode){
+export function findTagNameNodeInStack(nodeStack: Node[]): Node {
+  const elementNode = nodeStack.findLast((n) => n.kind === SyntaxKind.ElementNode);
+  if (!elementNode) {
     return null;
   }
 
-  const tagNameNode =  elementNode.children!.find(n => n.kind === SyntaxKind.TagNameNode);
+  const tagNameNode = elementNode.children!.find((n) => n.kind === SyntaxKind.TagNameNode);
   return tagNameNode;
 }
 
 export function compNameForTagNameNode(tagNameNode: Node, getText: GetText): string | null {
   const colonIdx = tagNameNode.children!.findIndex((n) => n.kind === SyntaxKind.Colon);
-    const hasNs = colonIdx === -1 ? false : tagNameNode.children!.slice(0, colonIdx).findIndex((n:Node) => n.kind === SyntaxKind.Identifier) !== -1;
-    if(hasNs){
-      return null;
-    }
-    const nameNode = tagNameNode.children!.findLast(c => c.kind === SyntaxKind.Identifier)
-    return getText(nameNode);
+  const hasNs =
+    colonIdx === -1
+      ? false
+      : tagNameNode
+          .children!.slice(0, colonIdx)
+          .findIndex((n: Node) => n.kind === SyntaxKind.Identifier) !== -1;
+  if (hasNs) {
+    return null;
+  }
+  const nameNode = tagNameNode.children!.findLast((c) => c.kind === SyntaxKind.Identifier);
+  return getText(nameNode);
 }
 
 /**
-*
-* @param pathToElementNode nodes from the inner most node to the closest ElementNode
-* @returns
-*/
-export function insideClosingTag(pathToElementNode:Node[]): boolean {
-  if(pathToElementNode === null){
+ *
+ * @param pathToElementNode nodes from the inner most node to the closest ElementNode
+ * @returns
+ */
+export function insideClosingTag(pathToElementNode: Node[]): boolean {
+  if (pathToElementNode === null) {
     return false;
   }
 
   const elementNode = pathToElementNode.at(-1)!;
   const nodeBeforeElementNode = pathToElementNode.at(-2);
-  const inputWasOnlyAnElementNode = !nodeBeforeElementNode
+  const inputWasOnlyAnElementNode = !nodeBeforeElementNode;
 
-  if (inputWasOnlyAnElementNode){
+  if (inputWasOnlyAnElementNode) {
     return false;
   }
 
-  const idxClosingStartToken = elementNode.children!.findIndex((n) => n.kind === SyntaxKind.CloseNodeStart);
-  if(idxClosingStartToken === -1){
+  const idxClosingStartToken = elementNode.children!.findIndex(
+    (n) => n.kind === SyntaxKind.CloseNodeStart,
+  );
+  if (idxClosingStartToken === -1) {
     return false;
   }
 
-  const idxElementNodeChild = elementNode.children!.findIndex((n) => n === nodeBeforeElementNode)
-  if(idxClosingStartToken <= idxElementNodeChild ){
+  const idxElementNodeChild = elementNode.children!.findIndex((n) => n === nodeBeforeElementNode);
+  if (idxClosingStartToken <= idxElementNodeChild) {
     return true;
   }
 
@@ -55,23 +61,26 @@ export function insideClosingTag(pathToElementNode:Node[]): boolean {
 }
 
 /**
-* Visits ancestors in the chain from the innermost to the outermost node.
-* @param chain The chain of nodes to visit.
-* @param predicate A function that takes a node and returns a boolean.
-* @returns An array of nodes from the innermost to the node that satisfies the predicate, or null if no such node is found.
-*/
-export function visitAncestorsInChain(chain: Node[], predicate: (node: Node) => boolean): Node[] | null {
+ * Visits ancestors in the chain from the innermost to the outermost node.
+ * @param chain The chain of nodes to visit.
+ * @param predicate A function that takes a node and returns a boolean.
+ * @returns An array of nodes from the innermost to the node that satisfies the predicate, or null if no such node is found.
+ */
+export function visitAncestorsInChain(
+  chain: Node[],
+  predicate: (node: Node) => boolean,
+): Node[] | null {
   let currentIdx = -1;
   let current = chain.at(currentIdx);
   let path = [current];
 
-  while(current) {
-    if(predicate(current)) {
+  while (current) {
+    if (predicate(current)) {
       return path;
     }
     currentIdx--;
     current = chain.at(currentIdx);
-    path.push(current)
+    path.push(current);
   }
   return null;
 }
@@ -108,14 +117,14 @@ export function getAllRelevantNodesFromAncestorChain(chain: Node[], kind: Syntax
 }
 
 /**
-*
-* @param node an ElementNode
-*/
-function isPairedNode(node: Node): boolean{
-  for (const c of node.children){
-    if (c.kind === SyntaxKind.CloseNodeStart){
+ *
+ * @param node an ElementNode
+ */
+function isPairedNode(node: Node): boolean {
+  for (const c of node.children) {
+    if (c.kind === SyntaxKind.CloseNodeStart) {
       return true;
-    } else if (c.kind === SyntaxKind.NodeClose){
+    } else if (c.kind === SyntaxKind.NodeClose) {
       return false;
     }
   }
@@ -123,19 +132,9 @@ function isPairedNode(node: Node): boolean{
 }
 
 /**
-*
-* @param node an ElementNode
-*/
-function isSelfClosingNode(node: Node): boolean{
+ *
+ * @param node an ElementNode
+ */
+function isSelfClosingNode(node: Node): boolean {
   return !isPairedNode(node);
-}
-
-export function getTriviaNodes(node: Node): Node[] {
-  if (node.pos === node.start){
-    return [];
-  } else if (node.triviaBefore){
-    return node.triviaBefore;
-  } else {
-    return getTriviaNodes(node.children[0]);
-  }
 }
