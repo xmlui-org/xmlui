efa76e8b9f61a63f129dd9bdb36b657a17a172b1
marcello-tk <awesomedevguynull@gmail.com>
2025-10-20 17:55:02 +0200

    wip

--- xmlui/src/components/Table/TableNative.tsx
+++ xmlui/src/components/Table/TableNative.tsx
@@ -511,7 +511,6 @@ export const Table = forwardRef(
     ]);
 
     // --- Set up page information (using the first page size option)
-    // const [pagination, setPagination] = useState<PaginationState>();
     const [pagination, setPagination] = useState<PaginationState>({
       pageSize: isPaginated ? pageSize : Number.MAX_VALUE,
       pageIndex: currentPageIndex,
