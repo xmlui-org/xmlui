88dba6e269c9635dd715e046d3ebcad6dfac799e
captaaint <56363291+captaaint@users.noreply.github.com>
2025-10-28 08:25:11 +0100

    wip

--- packages/xmlui-playground/src/state/store.ts
+++ packages/xmlui-playground/src/state/store.ts
@@ -329,7 +329,6 @@ export const playgroundReducer = produce((state: PlaygroundState, action: Playgr
     case PlaygroundActionKind.TEXT_CHANGED:
       state.options.id = state.options.id + 1;
       {
-        console.log("abababababa");
         state.text = action.payload.text || "";
         state.error = null;
         if (state.options.content === "app") {
