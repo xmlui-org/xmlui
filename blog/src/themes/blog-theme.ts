3118b4f89bd7aa63905843d4137f02a595c3799b
captaaint <56363291+captaaint@users.noreply.github.com>
2025-11-12 15:02:59 +0100

    refactor: theme updates for the blog (#2355)

--- blog/src/themes/blog-theme.ts
+++ blog/src/themes/blog-theme.ts
@@ -1,15 +1,22 @@
 import type { ThemeDefinition } from "xmlui";
 
-export const DefaultDocsTheme: ThemeDefinition = {
+export const BlogTheme: ThemeDefinition = {
   name: "XMLUI Blog Theme",
   id: "blog-theme",
   extends: ["xmlui"],
   themeVars: {
-    "paddingHorizontal-Pages": "0px",
     // --- App layout
     "maxWidth-content-App": "800px",
+    // --- Fundamental colors & typography
+    backgroundColor: "$color-surface-0",
+    "color-primary": "#3367CC",
+    "color-surface": "#1e2734",
+    "fontWeight-Text": "400",
+    "fontWeight-bold": "700",
+    "textColor-primary": "$color-surface-800",
+    "textColor-NavLink--active": "$color-primary",
   },
   resources: {},
 };
 
-export default DefaultDocsTheme;
+export default BlogTheme;
