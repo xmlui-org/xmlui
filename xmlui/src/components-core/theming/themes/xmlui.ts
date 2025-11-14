620a0649aefdb068ba9fcd5c304370e6a1a583f7
captaaint <56363291+captaaint@users.noreply.github.com>
2025-11-14 09:38:12 +0100

    feat: add color property (that represents the theme) to theme definitions and use it in themeSwitcher (playground) (#2370)

--- xmlui/src/components-core/theming/themes/xmlui.ts
+++ xmlui/src/components-core/theming/themes/xmlui.ts
@@ -13,6 +13,7 @@ export const XmlUiThemeDefinition: ThemeDefinition = {
   resources: {
     // "font.inter": "https://rsms.me/inter/inter.css",
   },
+  color: "$color-primary-500",
   themeVars: {
     "font-size": "15px",
     "boxShadow-Input": "$boxShadow-sm",
@@ -35,54 +36,63 @@ export const XmlUiThemeDefinition: ThemeDefinition = {
 export const XmlUiGreenThemeDefinition: ThemeDefinition = {
   id: "xmlui-green",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: { ...greenThemeColors },
 };
 
 export const XmlUiGrayThemeDefinition: ThemeDefinition = {
   id: "xmlui-gray",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: { ...grayThemeColors },
 };
 
 export const XmlUiOrangeThemeDefinition: ThemeDefinition = {
   id: "xmlui-orange",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: { ...orangeThemeColors },
 };
 
 export const XmlUiPurpleThemeDefinition: ThemeDefinition = {
   id: "xmlui-purple",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: { ...purpleThemeColors },
 };
 
 export const XmlUiCyanThemeDefinition: ThemeDefinition = {
   id: "xmlui-cyan",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: { ...cyanThemeColors },
 };
 
 export const XmlUiRedThemeDefinition: ThemeDefinition = {
   id: "xmlui-red",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: { ...redThemeColors },
 };
 
 export const XmlUiDocsThemeDefinition: ThemeDefinition = {
   id: "xmlui-docs",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: {},
 };
 
 export const XmlUiBlogThemeDefinition: ThemeDefinition = {
   id: "xmlui-blog",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: {},
 };
 
 export const XmlUiWebThemeDefinition: ThemeDefinition = {
   id: "xmlui-web",
   extends: "xmlui",
+  color: "$color-primary-500",
   themeVars: {
     // --- Fundamental colors & typography
     "maxWidth-content-AppHeader": "1280px",
