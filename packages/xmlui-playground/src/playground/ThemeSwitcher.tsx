cf77867dead7e2c5796ea8d9f6b5b102e5d81407
captaaint <56363291+captaaint@users.noreply.github.com>
2025-11-14 09:35:25 +0100

    feat: add color property to theme definitions and filter themes in ThemeSwitcher

--- packages/xmlui-playground/src/playground/ThemeSwitcher.tsx
+++ packages/xmlui-playground/src/playground/ThemeSwitcher.tsx
@@ -2,11 +2,12 @@ import { usePlayground } from "../hooks/usePlayground";
 import { MdOutlinePalette } from "react-icons/md";
 import { activeThemeChanged } from "../state/store";
 import { forwardRef } from "react";
-import { Button, DropdownMenu, Icon, MenuItem, Text, Tooltip } from "xmlui";
+import { Button, DropdownMenu, Icon, MenuItem, Text, Theme, Tooltip, useThemes } from "xmlui";
 import styles from "./ThemeSwitcher.module.scss";
 
 export const ThemeSwitcher = forwardRef<HTMLButtonElement>(() => {
   const { appDescription, options, dispatch } = usePlayground();
+  const { themes } = useThemes();
 
   return (
     <Tooltip text="Change theme">
@@ -18,15 +19,26 @@ export const ThemeSwitcher = forwardRef<HTMLButtonElement>(() => {
         </Text>
         {appDescription.availableThemes &&
           appDescription.availableThemes.length > 0 &&
-          appDescription.availableThemes.map((theme, index) => (
-            <MenuItem
-              iconPosition="end"
-              icon={theme.id === options.activeTheme ? <Icon name="checkmark" /> : undefined}
-              key={index}
-              label={theme.id}
-              onClick={() => dispatch(activeThemeChanged(theme.id))}
-            />
-          ))}
+          appDescription.availableThemes
+            .filter(
+              (theme) =>
+                theme.id !== "xmlui-docs" && theme.id !== "xmlui-blog" && theme.id !== "xmlui-web",
+            )
+            .map((theme, index) => (
+              <Theme
+                key={index}
+                id={!themes.find((t) => t.id === theme.id)?.id ? "xmlui" : theme.id}
+                themeVars={{ "color-MenuItem": theme.color || "$color-primary-500" }}
+              >
+                <MenuItem
+                  iconPosition="end"
+                  icon={theme.id === options.activeTheme ? <Icon name="checkmark" /> : undefined}
+                  key={index}
+                  label={theme.id}
+                  onClick={() => dispatch(activeThemeChanged(theme.id))}
+                />
+              </Theme>
+            ))}
       </DropdownMenu>
     </Tooltip>
   );
