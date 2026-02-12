e2dbeb7d7ffb6c98c20eb21bc996d39d47be41a0
captaaint <56363291+captaaint@users.noreply.github.com>
2026-02-12 19:59:31 +0100

    wip

--- docs/extensions.ts
+++ docs/extensions.ts
@@ -1,5 +1,6 @@
 import playground from "xmlui-playground";
 import search from "xmlui-search";
 import helloWorld from "xmlui-hello-world";
+import websiteBlocks from "xmlui-website-blocks";
 
-export default [playground, search, helloWorld];
+export default [playground, search, helloWorld, websiteBlocks];
