da4a6fbe68d9b3bd32609421cecaff41f8965934
Marcell Kiss <52428841+marcello-tk@users.noreply.github.com>
2026-02-23 22:10:36 +0100

    fix: marked Text variant inline padding (#2883)

--- xmlui/src/components/Text/Text.tsx
+++ xmlui/src/components/Text/Text.tsx
@@ -194,8 +194,7 @@ export const TextMd = createMetadata({
     [`fontWeight-${COMP}-strong`]: "$fontWeight-bold",
 
     [`backgroundColor-${COMP}-marked`]: "rgb(from $color-primary-200 r g b / 0.4)",
-    [`paddingHorizontal-${COMP}-marked`]: "$space-1",
-
+    
     dark: {
       [`backgroundColor-${COMP}-marked`]: "rgb(from $color-primary-400 r g b / 0.4)",
     },
