18bc3c69ca56b3d9fb1bf88d4a93df57335108bc
captaaint <56363291+captaaint@users.noreply.github.com>
2026-02-24 12:19:28 +0100

    wip

---
+++ website/src/themes/xmlui-green-on-default.ts
@@ -0,0 +1,11 @@
+import type { ThemeDefinition } from "xmlui";
+
+export const XmluiGreenOnDefaultTheme: ThemeDefinition = {
+  name: "XMLUI Green Documentation Theme",
+  id: "xmlui-green-on-default",
+  extends: ["xmlui-green", "xmlui-docs"],
+  themeVars: {},
+  resources: {},
+};
+
+export default XmluiGreenOnDefaultTheme;
