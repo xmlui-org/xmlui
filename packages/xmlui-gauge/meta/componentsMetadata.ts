c495d0f9166356c2557513bef938a4dfc1f727ec
Istvan Novak <dotneteer@hotmail.com>
2026-03-23 11:13:09 +0200

    Generate documentation for all extensions (#3105)

---
+++ packages/xmlui-gauge/meta/componentsMetadata.ts
@@ -0,0 +1,10 @@
+import { GaugeMd } from "../src/GaugeWrapped";
+
+export const componentMetadata = {
+  name: "Gauge",
+  state: "experimental",
+  description: "This package provides a circular gauge component for displaying numeric values with full XMLUI theming integration.",
+  metadata: {
+    Gauge: GaugeMd,
+  },
+};
