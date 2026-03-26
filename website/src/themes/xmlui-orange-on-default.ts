a50b41b1576c2bb1af46d62c592a9ac4887c6153
Istvan Novak <dotneteer@hotmail.com>
2026-03-26 06:41:51 +0100

    Fix the Color Palettes docs article

--- website/src/themes/xmlui-orange-on-default.ts
+++ website/src/themes/xmlui-orange-on-default.ts
@@ -3,7 +3,7 @@ import type { ThemeDefinition } from "xmlui";
 export const XmluiOrangeOnDefaultTheme: ThemeDefinition = {
   name: "XMLUI Orange Documentation Theme",
   id: "xmlui-orange-on-default",
-  extends: ["xmlui-orange", "xmlui-docs"],
+  extends: ["xmlui-orange", "xmlui-website-theme"],
   themeVars: {},
   resources: {},
 };
