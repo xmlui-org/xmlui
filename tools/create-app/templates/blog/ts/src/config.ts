4e45532e8b55c65479334d175553a3730339cdcd
Tamas Kapitany <56363291+captaaint@users.noreply.github.com>
2026-05-05 15:41:30 +0200

    feat: add docs and blog templates to create-xmlui-app (#3439)

---
+++ tools/create-app/templates/blog/ts/src/config.ts
@@ -0,0 +1,12 @@
+import type { StandaloneAppDescription } from "xmlui";
+
+const App: StandaloneAppDescription = {
+  name: "My Blog",
+  version: "0.1.0",
+  resources: {
+    logo: "resources/xmlui-logo.svg",
+    favicon: "resources/favicon.ico",
+  },
+};
+
+export default App;
