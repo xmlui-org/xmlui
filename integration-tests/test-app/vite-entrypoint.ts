5dd1f88751c11ffb6665d82d8fe06490bb18176d
Norbert Hajagos <hajagosnorbi@gmail.com>
2026-05-05 20:02:44 +0200

    fix: import stmt, delete planning artifacts

--- integration-tests/test-app/vite-entrypoint.ts
+++ integration-tests/test-app/vite-entrypoint.ts
@@ -1,7 +1,7 @@
 import { startApp } from "xmlui";
 import extensions from "./extensions";
 
-const runtime = import.meta.glob(["./**/*.xmlui", "/config.json"], { eager: true });
+const runtime = import.meta.glob(["./**/*.xmlui"], { eager: true });
 
 export function init() {
   startApp(runtime, extensions);
