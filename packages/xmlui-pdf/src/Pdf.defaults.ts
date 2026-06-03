17c01521ce1836441890756598c401d67e96ec9b
Istvan Novak <dotneteer@hotmail.com>
2026-06-03 11:31:03 +0200

    wip: fix failing build

--- packages/xmlui-pdf/src/Pdf.defaults.ts
+++ packages/xmlui-pdf/src/Pdf.defaults.ts
@@ -1,7 +1,9 @@
+import type { Annotation } from "./types/annotation.types";
+
 export const defaultProps = {
   mode: "view" as "view" | "edit",
   scale: 1.0,
-  annotations: [] as unknown[],
+  annotations: [] as Annotation[],
   horizontalAlignment: "start" as "start" | "center" | "end",
   verticalAlignment: "start" as "start" | "center" | "end",
   scrollStyle: "normal" as "normal" | "overlay" | "whenMouseOver" | "whenScrolling",
