50660eb97aaa79d916b47e00d323ecc476bdb0bb
Istvan Novak <dotneteer@hotmail.com>
2026-07-29 09:27:03 +0200

    Fix new SSG regressions

--- xmlui/tests/nodejs/ssg.test.ts
+++ xmlui/tests/nodejs/ssg.test.ts
@@ -4,10 +4,10 @@ import { describe, expect, it } from "vitest";
 import { getSsgEntrySource } from "../../src/nodejs/bin/ssg";
 
 describe("getSsgEntrySource", () => {
-  it("imports server routing APIs from react-router", () => {
+  it("imports server routing APIs from react-router-dom/server", () => {
     const source = getSsgEntrySource([]);
 
-    expect(source).toContain('import { StaticRouter } from "react-router";');
-    expect(source).not.toContain("react-router-dom/server");
+    expect(source).toContain('import { StaticRouter } from "react-router-dom/server";');
+    expect(source).not.toContain('import { StaticRouter } from "react-router";');
   });
 });
